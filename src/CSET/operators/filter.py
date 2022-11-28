35998e2985c8a32ec98ea797cc406237bdd0285e
jwarner8 <james.warner@metoffice.gov.uk>
2022-11-28 11:40:58 +0000

    update filter

--- src/CSET/operators/filter.py
+++ src/CSET/operators/filter.py
@@ -4,33 +4,37 @@ Operators to perform various kind of filtering.
 # This file probably wants renaming as it clashes with the builtin filter
 # function.
 
-from iris.cube import CubeList
+import iris.cube
 
 
-def extract_field(cube: CubeList, field: str) -> CubeList:
+def filter_cubes(cubelist, stash, methodconstraint=None, levelconstraint=None):
+
     """
-    Returns a cube containing only the specified field.
+    Arguments
+    ---------
 
-    Parameters
-    ----------
-    cube: Cube
-        An iris cube of the data to average.
-
-    field: str
-        The variable to keep.
+    * **cubelist**          - an iris.cube.CubeList containing cubes to iterate over.
+    * **stash**             - an string containing the stash code to extract.
+    * **methodconstraint**  - optional, an iris.constraint.
+    * **levelconstraint**   - optional, an iris.constraint.
 
     Returns
     -------
-    Cube
-        A cube containing the averaged field.
+    * **cubes** - an iris.cube.CubeList 
 
-    Raises
-    ------
-    IndexError
-        If the cube does not contain `field`.
-
-    Notes
-    -----
-    Not yet implemented.
     """
-    pass
+
+    # initialise empty cubelist to append filtered cubes too
+    filtered_cubes = iris.cube.CubeList()
+
+    # Iterate over all cubes
+    for cube in cubelist:
+        if cube.attributes['STASH'] == stash:
+            filtered_cubes.append(cube)    
+
+    #TODO - while okay for this example, we need to be able to futher filter
+    #cubes by 'cell_methods', levels etc. Some stash will return multiple cubes
+    #with different time processing. E.g. wind gust stash (m01s03i463) has a 
+    #cell method (CellMethod(method='maximum', coord_names=('time',), intervals=('1 hour',), comments=()),)
+
+    return filtered_cubes
