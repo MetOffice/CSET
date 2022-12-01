f12e4b04339f455265dc3535d0fbe18b82c1a124
jwarner8 <james.warner@metoffice.gov.uk>
2022-12-01 10:25:18 +0000

    change write args and extension checker

--- src/CSET/operators/write.py
+++ src/CSET/operators/write.py
@@ -5,28 +5,30 @@ Operators for writing various types of files to disk.
 import iris
 
 
-def write_cubelist_to_nc(cubelist, saver):
+def write_cube_to_nc(cube, saver):
 
     """
     A write operator that sits after the read operator. This operator expects
-    a iris.cube.CubeList object containing filtered cubes that will then be
-    passed to MET for further processing.
+    a iris.cube object that will then be passed to MET for further processing.
 
     Arguments
     ---------
 
-    * **cubelist**  - an iris.cube.CubeList object containing cube(s).
+    * **cube**  - an iris.cube
     * **saver** - a string containing the path to save the cubes too
 
     Returns
     -------
-    * **saver+'.nc'** - filepath to saved .nc
+    * **saver** - filepath to saved .nc
 
     """
 
-    # Save the file as nc compliant (iris should handle this). Append .nc to
-    # ensure nc format.
-    iris.save(cubelist, saver+'.nc')
+    # Append .nc to saver path
+    if not saver.endswith('.nc'):
+        saver = saver+'.nc'
 
-    return saver+'.nc'
+    # Save the file as nc compliant (iris should handle this)
+    iris.save(cube, saver)
+
+    return saver
 
