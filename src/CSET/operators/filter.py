62cadbf6eabcbcb97a8cf64fbdd2b40fe3f9b33a
jwarner8 <james.warner@metoffice.gov.uk>
2022-12-01 12:24:41 +0000

    replace str with attribute constraint

--- src/CSET/operators/filter.py
+++ src/CSET/operators/filter.py
@@ -26,12 +26,14 @@ def filter_cubes(cubelist: iris.cube.CubeList, stash: str, methodconstraint: tup
     # initialise empty cubelist to append filtered cubes too
     filtered_cubes = iris.cube.CubeList()
 
+    # Initialise stash constraint
+    stash_constraint = iris.AttributeConstraint(STASH=stash)
+
     # Iterate over all cubes and check stash/cell method
     # TODO - need to add additional filtering/checking i.e. time/accum, pressure level...
-    for cube in cubelist:
-        if cube.attributes['STASH'] == stash:
-            if cube.cell_methods == methodconstraint:
-                filtered_cubes.append(cube)
+    for cube in cubelist.extract(stash_constraint):
+        if cube.cell_methods == methodconstraint:
+            filtered_cubes.append(cube)
 
     # Check filtered cubes is a cubelist containing one cube.
     if len(filtered_cubes) == 1:
