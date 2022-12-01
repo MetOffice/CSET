bc5947f6941f4a871c364b1cbf73debecf84f0d8
jwarner8 <james.warner@metoffice.gov.uk>
2022-12-01 12:12:45 +0000

    add stash filter to read, type hints

--- src/CSET/operators/read.py
+++ src/CSET/operators/read.py
@@ -3,18 +3,21 @@ Operators for reading various types of files from disk.
 """
 
 import iris
+from pathlib import Path
 
 
-def read_cubes(loadpath):
+def read_cubes(loadpath: Path, stash: str) --> iris.cube.CubeList:
 
     """
     Read operator that takes a path string (can include wildcards), and uses
-    iris to load all the cubes and return an iris.cube.CubeList object.
+    iris to load all the cubes matching stash and return an 
+    iris.cube.CubeList object.
 
     Arguments
     ---------
 
-    * **loadpath**  - an string containing a path to where .pp files are located.
+    * **loadpath**  - string containing a path to where .pp files are located.
+    * **stash**     - string containing stash code to filter
 
     Returns
     -------
@@ -24,4 +27,4 @@ def read_cubes(loadpath):
 
     #TODO: validation that data exists
 
-    return iris.load(loadpath)
+    return iris.load(loadpath, stash)
