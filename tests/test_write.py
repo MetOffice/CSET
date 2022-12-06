7e3fa5bfd2d39ee1586c6cd75b2fb0154c0be14f
James Frost <james.frost@metoffice.gov.uk>
2022-12-06 11:39:31 +0000

    Add test case for writing to a string as well as a Path.

--- tests/test_write.py
+++ tests/test_write.py
@@ -4,11 +4,14 @@ from pathlib import Path
 
 
 def test_write_cube_to_nc():
-    """Write cube and verify."""
+    """Write cube to Path and string path and verify."""
     cubes = read.read_cubes("tests/test_data/air_temp.nc")
     cube = filters.filter_cubes(cubes, "m01s03i236", ())
-    filename = Path(f"/tmp/{token_hex(4)}write_test_cube.nc")
+    filename = f"/tmp/{token_hex(4)}_write_test_cube.nc"
+    filepath = Path(f"/tmp/{token_hex(4)}_write_test_cube.nc")
+    write.write_cube_to_nc(cube, filepath)
     write.write_cube_to_nc(cube, filename)
-    written_cube = read.read_cubes(filename)[0]
+    written_cube = read.read_cubes(filepath)[0]
     assert written_cube == cube
-    filename.unlink()
+    filepath.unlink()
+    Path(filename).unlink()
