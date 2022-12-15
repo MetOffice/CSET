dcd70038e9e94175b18aeb10cc1c770c404d1367
Sylviabohnenstengel <Sylvia.bohnenstengel@metoffice.gov.uk>
2022-12-15 19:22:03 +0000

    adding functions to built stash and variable constraints incl tests

--- src/CSET/run-operators.py
+++ src/CSET/run-operators.py
@@ -10,13 +10,18 @@ argument.
 """
 
 import sys
-from CSET.operators import read, write, filters
+from CSET.operators import generate_constraints, read, write, filters
 
 # First argument is input file name, second is output file name.
 input_file = sys.argv[1]
 output_file = sys.argv[2]
+stash = sys.argv[3] # "m01s03i236" for test purposes
+varname = sys.argv[4] # "test" for test purposes
 
 # Hardcoded task chain to extract instantaneous air temperature.
-cubes = read.read_cubes(input_file)
+stash_constraint = generate_constraints.generate_stash_constraints(stash)
+var_constraint = generate_constraints.generate_var_constraints(varname)
+cubes = read.read_cubes(input_file, stash_constraint)
 cube = filters.filter_cubes(cubes, "m01s03i236", ())
 write.write_cube_to_nc(cube, output_file)
+
