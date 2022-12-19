10bfc83b43268c9a850bd52d8388fb9a9020a800
Sylviabohnenstengel <Sylvia.bohnenstengel@metoffice.gov.uk>
2022-12-19 20:20:48 +0000

    test_generate_constraints now updated.Seems not to have updated in previous commit

--- tests/test_generate_constraints.py
+++ tests/test_generate_constraints.py
@@ -4,11 +4,14 @@ from CSET.operators import generate_constraints
 def test_generate_constraints_operator():
     """generate iris cube constraint for UM STASH code."""
     stash_constraint = generate_constraints.generate_stash_constraints("m01s03i236")
-    assert type(stash_constraint) == "<class 'iris._constraints.AttributeConstraint'>"
-
+    #    expected_stash_constraint = "<class 'iris._constraints.AttributeConstraint'>"
+    #    assert type(stash_constraint) == expected_stash_constraint
+    expected_stash_constraint = "AttributeConstraint({'STASH': 'm01s03i236'})"
+    assert repr(stash_constraint) == expected_stash_constraint
+    
     """generate iris cube constraint for str variable name."""
     var_constraint = generate_constraints.generate_var_constraints("test")
-    assert type(var_constraint) == "<class 'iris._constraints.Constraint'>"
-
-
-    
+    # expected_var_constraint = "<class 'iris._constraints.Constraint'>"
+    # assert type(var_constraint) == expected_var_constraint
+    expected_var_constraint = "Constraint(name='test')"
+    assert repr(var_constraint) == expected_var_constraint
