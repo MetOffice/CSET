01b6e51ce218dfc11da4a3cdb49573840d59fc67
Sylviabohnenstengel <Sylvia.bohnenstengel@metoffice.gov.uk>
2022-12-19 20:13:24 +0000

    layout changes

--- src/CSET/operators/generate_constraints.py
+++ src/CSET/operators/generate_constraints.py
@@ -5,10 +5,8 @@ Operators to generate load constraints and pass into read operator.
 import iris
 import iris.cube
 
-# argument should be a list of stash codes that combined build 
-# the constraint
 def generate_stash_constraints(stash: str) -> iris.AttributeConstraint:
-
+    
     """
     Operator that takes a stash string, 
     and uses iris to generate a constraint to be 
@@ -16,51 +14,50 @@ def generate_stash_constraints(stash: str) -> iris.AttributeConstraint:
     read operator loads and speed up loading.
     This is not replacing the more fine grained filter operator.
     At a later stage str list required to combine constraints.
+    Arargument should be a list of stash codes that combined build 
+    the constraint.
 
-   Arguments
+    Arguments
     ---------
     stash: str 
         stash code to build iris constrain
-
+    
     Returns
     -------
     stash_constraint: iris.AttributeConstraint
-
+    
     """
-
-    # Need to load stash codes as type iris.Attribute as well as names as 
+    
+    # Load stash codes as type iris.Attribute as well as names as 
     # iris.Constraint
     if type(stash) == str:
         stash_constraint = iris.AttributeConstraint(STASH=stash)
         return stash_constraint
     else:
         print('Further constraint conditions required...')
+        
+        
+    def generate_var_constraints(varname: str) -> iris.Constraint:
 
-
-
-# argument should be a list of stash codes that combined build 
-# the constraint
-def generate_var_constraints(varname: str) -> iris.Constraint:
-
-    """
-    Operator that takes a CF compliant variable name string, 
-    and uses iris to generate a constraint to be 
-    passed into the read operator to minimize the CubeList the 
-    read operator loads and speed up loading.
-    This is not replacing the more fine grained filter operator.
-    At a later stage str list required to combine constraints.
-
-   Arguments
-    ---------
-    varname: str
-        CF compliant name of variable. Needed later for LFRic.
-
-    Returns
-    -------
-    varname_constraint: iris.Constraint 
-
-    """
-
+        """
+        Operator that takes a CF compliant variable name string, 
+        and uses iris to generate a constraint to be 
+        passed into the read operator to minimize the CubeList the 
+        read operator loads and speed up loading.
+        This is not replacing the more fine grained filter operator.
+        At a later stage str list required to combine constraints.
+        
+        Arguments
+        ---------
+        varname: str
+            CF compliant name of variable. Needed later for LFRic.
+        
+        Returns
+        -------
+        varname_constraint: iris.Constraint 
+        
+        """
+        
     # Need to load stash codes as type iris.Attribute as well as 
     # names as iris.Constraint
     if type(varname) == str:
@@ -68,4 +65,4 @@ def generate_var_constraints(varname: str) -> iris.Constraint:
         return varname_constraint
     else:
         print('Further constraint conditions required...')
-
+        
