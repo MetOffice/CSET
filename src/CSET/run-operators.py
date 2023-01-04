a1a191a2ead8e234f0f9435993141603f8c22b17
James Frost <james.frost@metoffice.gov.uk>
2023-01-04 10:46:13 +0000

    Migrate to argsparse for CLI options

--- src/CSET/run-operators.py
+++ src/CSET/run-operators.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-# Copyright 2022 Met Office and contributors.
+# Copyright 2022-2023 Met Office and contributors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,30 +19,22 @@ Script to chain together individual operators.
 
 Currently it is hard coded, but in future it will take an argument specifying a
 config file describing what operators to run in what order.
-
-It takes the input file as its first argument, and the output file as the second
-argument. The third argument is stash name (currently "m01s03i236", and
-optional) and forth argument is variable name (currently "test", and optional)
-to constrain loading of model data.
 """
 
-import sys
+import argparse
+
+parser = argparse.ArgumentParser(description="Run an operator chain.")
+parser.add_argument("input_file", help="input file to read")
+parser.add_argument("output_file", help="output file to write")
+parser.add_argument("--stash", help="STASH code to filter by", default="m01s03i236")
+parser.add_argument("--varname", help="variable name to filter by", default="test")
+args = parser.parse_args()
+
 from CSET.operators import generate_constraints, read, write, filters
 
-input_file = sys.argv[1]
-output_file = sys.argv[2]
-try:
-    stash = sys.argv[3]
-except IndexError:
-    stash = "m01s03i236"
-try:
-    varname = sys.argv[4]
-except IndexError:
-    varname = "test"
-
 # Hardcoded task chain to extract instantaneous air temperature.
-stash_constraint = generate_constraints.generate_stash_constraints(stash)
-var_constraint = generate_constraints.generate_var_constraints(varname)
-cubes = read.read_cubes(input_file, stash_constraint)
-cube = filters.filter_cubes(cubes, "m01s03i236", ())
-write.write_cube_to_nc(cube, output_file)
+stash_constraint = generate_constraints.generate_stash_constraints(args.stash)
+var_constraint = generate_constraints.generate_var_constraints(args.varname)
+cubes = read.read_cubes(args.input_file, stash_constraint)
+cube = filters.filter_cubes(cubes, args.stash, ())
+write.write_cube_to_nc(cube, args.output_file)
