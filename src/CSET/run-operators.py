951766c9fa920fd8b892cf2bf14110f7b8b81cae
James Frost <james.frost@metoffice.gov.uk>
2023-01-04 16:28:47 +0000

    Code outline for moving to recipe files added

--- src/CSET/run-operators.py
+++ src/CSET/run-operators.py
@@ -22,19 +22,45 @@ config file describing what operators to run in what order.
 """
 
 import argparse
+from pathlib import Path
 
-parser = argparse.ArgumentParser(description="Run an operator chain.")
-parser.add_argument("input_file", help="input file to read")
-parser.add_argument("output_file", help="output file to write")
-parser.add_argument("--stash", help="STASH code to filter by", default="m01s03i236")
-parser.add_argument("--varname", help="variable name to filter by", default="test")
-args = parser.parse_args()
 
-from CSET.operators import generate_constraints, read, write, filters
+def parse_recipe(recipe: str):
+    """
+    Parses the operator recipe and returns a function that runs the required
+    operators.
+    """
+    if recipe is None:
+        return hardcoded_recipe
+    else:
+        raise NotImplementedError
 
-# Hardcoded task chain to extract instantaneous air temperature.
-stash_constraint = generate_constraints.generate_stash_constraints(args.stash)
-var_constraint = generate_constraints.generate_var_constraints(args.varname)
-cubes = read.read_cubes(args.input_file, stash_constraint)
-cube = filters.filter_cubes(cubes, args.stash, ())
-write.write_cube_to_nc(cube, args.output_file)
+
+def hardcoded_recipe(input_file, output_file):
+    """
+    Hardcoded task chain to extract instantaneous air temperature.
+
+    TODO: Replace with recipe file.
+    """
+    from CSET.operators import generate_constraints, read, write, filters
+
+    stash = "m01s03i236"
+    # varname = "test"
+
+    stash_constraint = generate_constraints.generate_stash_constraints(stash)
+    # var_constraint = generate_constraints.generate_var_constraints(varname)
+    cubes = read.read_cubes(input_file, stash_constraint)
+    cube = filters.filter_cubes(cubes, stash, ())
+    write.write_cube_to_nc(cube, output_file)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run an operator chain.")
+    parser.add_argument("input_file", type=Path, help="input file to read")
+    parser.add_argument("output_file", type=Path, help="output file to write")
+    parser.add_argument("recipe", type=Path, help="recipe file to execute")
+    args = parser.parse_args()
+    with open(args.recipe, encoding="UTF-8") as f:
+        recipe = f.read()
+    operator_task = parse_recipe(recipe)
+    operator_task(args.input_file, args.output_file)
