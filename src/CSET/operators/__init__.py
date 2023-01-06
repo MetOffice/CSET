4d3c5ead42401ff547c2b48f5e8bcf0446de138b
James Frost <james.frost@metoffice.gov.uk>
2023-01-06 12:27:35 +0000

    Stop iris warning about datum support

--- src/CSET/operators/__init__.py
+++ src/CSET/operators/__init__.py
@@ -2,3 +2,8 @@
 This subpackage contains all of CSET's operators.
 """
 from CSET.operators import read, write, filters, generate_constraints
+
+import iris
+
+# Stop iris giving a warning whenever it loads something.
+iris.FUTURE.datum_support = True
