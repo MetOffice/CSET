6fdaabd427d8f05deb65419a2525984d5384fc3b
James Frost <james.frost@metoffice.gov.uk>
2023-01-06 11:14:53 +0000

    Revert "Correctly compare dict types in run-operators.py"
    This reverts commit efe2b57735be00d739e35c0707f17fb400001391.
    
    It was done correctly in the first place, but the error wasn't from it.

--- src/CSET/run-operators.py
+++ src/CSET/run-operators.py
@@ -53,7 +53,7 @@ class Recipe:
             if "args" in step:
                 args_string = []
                 for key in step["args"].keys():
-                    if type(step["args"][key]) == type({}):
+                    if type(step["args"][key]) == dict:
                         args_string.append(
                             f"{key} = {repr(step_parser(step['args'][key]))}"
                         )
@@ -63,7 +63,7 @@ class Recipe:
                         args_string.append(f"{key} = {repr(step['args'][key])}")
                 args = "".join(args_string)
             if "input" in step:
-                if type(step("input")) == type({}):
+                if type(step("input")) == dict:
                     step_input = repr(step_parser(step["input"]))
                 else:
                     step_input = repr(step["input"])
