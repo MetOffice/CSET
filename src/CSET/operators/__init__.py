1a10742afc72a13e90f5958c5986a2fff59656b3
James Frost <james.frost@metoffice.gov.uk>
2023-01-09 11:12:27 +0000

    Replace match-case and add docstring in get_operator
    match-case syntax was added in 3.10, and as we are aiming to work with 3.8 we
    can't use it.

--- src/CSET/operators/__init__.py
+++ src/CSET/operators/__init__.py
@@ -10,16 +10,38 @@ iris.FUTURE.datum_support = True
 
 
 def get_operator(name: str):
-    match name:
-        case "read.read_cubes":
-            return read.read_cubes
-        case "write.write_cube_to_nc":
-            return write.write_cube_to_nc
-        case "filters.filter_cubes":
-            return filters.filter_cubes
-        case "generate_constraints.generate_stash_constraints":
-            return generate_constraints.generate_stash_constraints
-        case "generate_constraints.generate_var_constraints":
-            return generate_constraints.generate_var_constraints
-        case _:
-            raise ValueError(f"Unknown operator: {name}")
+    """
+    Increments the input by one.
+
+    Parameters
+    ----------
+    name: str
+        The name of the desired operator.
+
+    Returns
+    -------
+    function
+        The named operator.
+
+    Raises
+    ------
+    ValueError
+        If name is not an operator.
+
+    Examples
+    --------
+    >>> CSET.operators.get_operator("read.read_cubes")
+    <function read_cubes at 0x7fcf9353c8b0>
+    """
+    if name == "read.read_cubes":
+        return read.read_cubes
+    if name == "write.write_cube_to_nc":
+        return write.write_cube_to_nc
+    if name == "filters.filter_cubes":
+        return filters.filter_cubes
+    if name == "generate_constraints.generate_stash_constraints":
+        return generate_constraints.generate_stash_constraints
+    if name == "generate_constraints.generate_var_constraints":
+        return generate_constraints.generate_var_constraints
+    else:
+        raise ValueError(f"Unknown operator: {name}")
