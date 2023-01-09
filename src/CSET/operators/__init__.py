3c91adeb6b4ca35c658b1cb17543b7fca39ca77c
James Frost <james.frost@metoffice.gov.uk>
2023-01-09 10:57:29 +0000

    Add helper function to get an operator from its name

--- src/CSET/operators/__init__.py
+++ src/CSET/operators/__init__.py
@@ -7,3 +7,19 @@ import iris
 
 # Stop iris giving a warning whenever it loads something.
 iris.FUTURE.datum_support = True
+
+
+def get_operator(name: str):
+    match name:
+        case "read.read_cubes":
+            return read.read_cubes
+        case "write.write_cube_to_nc":
+            return write.write_cube_to_nc
+        case "filters.filter_cubes":
+            return filters.filter_cubes
+        case "generate_constraints.generate_stash_constraints":
+            return generate_constraints.generate_stash_constraints
+        case "generate_constraints.generate_var_constraints":
+            return generate_constraints.generate_var_constraints
+        case _:
+            raise ValueError(f"Unknown operator: {name}")
