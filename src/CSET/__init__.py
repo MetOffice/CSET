e0312c38e62a64d008761124f5e5de27bf403ff6
James Frost <james.frost@metoffice.gov.uk>
2023-01-27 16:29:09 +0000

    Add console script entrypoint and refactor argument parsing
    This changes the way to run CSET to be a single command with subcommands
    for various bits, though only the operator runner has thusfar been converted.

--- src/CSET/__init__.py
+++ src/CSET/__init__.py
@@ -15,3 +15,45 @@
 """
 CSET: Convective Scale Evaluation Tool
 """
+
+import argparse
+import logging
+from pathlib import Path
+
+
+def main():
+    """CLI entrypoint."""
+    parser = argparse.ArgumentParser(
+        prog="cset", description="Convective Scale Evaluation Tool."
+    )
+    parser.add_argument(
+        "--verbose",
+        "-v",
+        action="count",
+        default=0,
+        help="increase output verbosity, may be specified multiple times",
+    )
+    # https://docs.python.org/3/library/argparse.html#sub-commands
+    subparsers = parser.add_subparsers(title="subcommands")
+    parser_operators = subparsers.add_parser(
+        "operators", help="run a chain of operators"
+    )
+    parser_operators.add_argument("input_file", type=Path, help="input file to read")
+    parser_operators.add_argument("output_file", type=Path, help="output file to write")
+    parser_operators.add_argument(
+        "recipe_file", type=Path, help="recipe file to execute"
+    )
+    parser_operators.set_defaults(func=run_operators)
+    args = parser.parse_args()
+    # Logging verbosity
+    if args.verbose >= 2:
+        logging.basicConfig(level=logging.DEBUG)
+    elif args.verbose >= 1:
+        logging.basicConfig(level=logging.INFO)
+    args.func(args)
+
+
+def run_operators(args):
+    from .operators._internal import execute_recipe
+
+    execute_recipe(args.recipe_file, args.input_file, args.output_file)
