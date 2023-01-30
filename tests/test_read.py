6f65dc95e3fce2460e9fb37d2bb76c155aeb1244
James Frost <james.frost@metoffice.gov.uk>
2023-01-30 10:23:20 +0000

    Fix tests
    Adapts the tests to work with the changed operator APIs.

--- tests/test_read.py
+++ tests/test_read.py
@@ -19,9 +19,9 @@ def test_read_cubes():
     """Read cube and verify."""
     cubes = read.read_cubes("tests/test_data/air_temp.nc")
     assert len(cubes) == 3
-    possible_cubes = [
+    expected_cubes = [
         "<iris 'Cube' of air_temperature / (K) (time: 2; grid_latitude: 17; grid_longitude: 13)>",
         "<iris 'Cube' of air_temperature / (K) (time: 3; grid_latitude: 17; grid_longitude: 13)>",
     ]
     for cube in cubes:
-        assert repr(cube) in possible_cubes
+        assert repr(cube) in expected_cubes
