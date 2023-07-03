6f28b5f9e6f1a68e53f6b444138a5377445c49cf
James Frost <james.frost@metoffice.gov.uk>
2023-07-03 15:18:14 +0100

    Add basic tests for graph genration

---
+++ tests/test_graph.py
@@ -0,0 +1,43 @@
+# Copyright 2022 Met Office and contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from pathlib import Path
+import subprocess
+import tempfile
+from uuid import uuid4
+
+import CSET.graph
+
+
+def test_save_graph():
+    """Tests generating a graph from a recipe file."""
+    CSET.graph.save_graph(Path("tests/test_data/plot_instant_air_temp.yaml"))
+
+
+def test_cli_interface():
+    """Generates a graph with the command line interface"""
+    # Normal run
+    subprocess.run(("cset", "graph", "tests/test_data/noop_recipe.yaml"), check=True)
+    # Run with details
+    subprocess.run(
+        ("cset", "graph", "--detailed", "tests/test_data/noop_recipe.yaml"), check=True
+    )
+    # Run with output path specified
+    output_file = Path(tempfile.gettempdir(), f"{uuid4()}.svg")
+    subprocess.run(
+        ("cset", "graph", "-o", "str(output_file)", "tests/test_data/noop_recipe.yaml"),
+        check=True,
+    )
+    assert output_file.exists()
+    output_file.unlink()
