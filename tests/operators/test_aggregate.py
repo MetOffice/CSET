7cf3daf9ced5b52a4ef18293c10ee66e59308980
James Frost <james.frost@metoffice.gov.uk>
2023-11-17 09:54:23 +0000

    Apply black reformat

--- tests/operators/test_aggregate.py
+++ tests/operators/test_aggregate.py
@@ -32,7 +32,9 @@ def test_aggregate():
     cube = filters.filter_cubes(cubes, constraint)
 
     # Test adding further coordinate.
-    aggregated_cube = aggregate.time_aggregate(cube, method="SUM", interval_iso=interval)
+    aggregated_cube = aggregate.time_aggregate(
+        cube, method="SUM", interval_iso=interval
+    )
 
     # Check if number of aux coords on aggregated cube is
     # by 1 greater than original cube.
