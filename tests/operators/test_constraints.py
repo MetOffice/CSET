6cfc64e05162bd698e636c4b453265c725cf60c7
Sylvia Bohnenstengel <62748926+Sylviabohnenstengel@users.noreply.github.com>
2023-11-21 16:29:58 +0000

    Update tests/operators/test_constraints.py
    Co-authored-by: James Frost <james.frost@metoffice.gov.uk>

--- tests/operators/test_constraints.py
+++ tests/operators/test_constraints.py
@@ -34,7 +34,7 @@ def test_generate_var_constraint():
 
 
 def test_generate_model_level_constraint():
-    """Generate iris cube constraint for str variable name."""
+    """Generate iris cube constraint for model level number."""
     var_constraint = constraints.generate_model_level_constraint("2")
     expected_model_level_constraint = (
         "Constraint(coord_values={'model_level_number': 2})"
