e973ac6b4b7aa1f1dfec9be23159387d6966182b
daflack <david.flack1@metoffice.gov.uk>
2023-12-15 08:55:18 +0000

    move convection test data into folder

--- tests/operators/test_convection.py
+++ tests/operators/test_convection.py
@@ -9,11 +9,11 @@ import CSET.operators.convection as convection
 
 def test_cape_ratio():
     """Compare with precalculated ratio."""
-    precalculated = iris.load_cube("tests/test_data/ECFlagB.nc")
-    precalculated_2 = iris.load_cube("tests/test_data/ECFlagB_2.nc")
-    SBCAPE = iris.load_cube("tests/test_data/SBCAPE.nc")
-    MUCAPE = iris.load_cube("tests/test_data/MUCAPE.nc")
-    MUCIN = iris.load_cube("tests/test_data/MUCIN.nc")
+    precalculated = iris.load_cube("tests/test_data/convection/ECFlagB.nc")
+    precalculated_2 = iris.load_cube("tests/test_data/convection/ECFlagB_2.nc")
+    SBCAPE = iris.load_cube("tests/test_data/convection/SBCAPE.nc")
+    MUCAPE = iris.load_cube("tests/test_data/convection/MUCAPE.nc")
+    MUCIN = iris.load_cube("tests/test_data/convection/MUCIN.nc")
     assert (
         convection.cape_ratio(SBCAPE, MUCAPE, MUCIN).data.all()
         == precalculated.data.all()
