bc920aba461b19c90bdc27caec72cff9502c9fd9
James Frost <james.frost@metoffice.gov.uk>
2023-12-21 12:07:35 +0000

    Add help text for more config settings

--- cset-workflow/app/send_email/bin/send-email.py
+++ cset-workflow/app/send_email/bin/send-email.py
@@ -16,13 +16,20 @@ def get_home_page_addr():
     if len(path) == 1:
         path = os.getenv("WEB_DIR").split("/Public/")
     if len(path) == 1:
-        raise RuntimeError("Cannot determine web address.")
-    return f"{os.getenv('WEB_ADDR').strip('/')}/{path[-1]}"
+        raise ValueError("Cannot determine web address.")
+    try:
+        base_address = os.getenv("WEB_ADDR", "").strip("/")
+    except AttributeError as err:
+        raise ValueError("WEB_ADDR not set.") from err
+    return f"{base_address}/{path[-1]}"
 
 
 if __name__ == "__main__":
     subject = "CSET webpage ready"
-    body = f"""The webpage for your run of CSET is now ready. You can view it here:\n{get_home_page_addr()}"""
+    try:
+        body = f"The webpage for your run of CSET is now ready. You can view it here:\n{get_home_page_addr()}"
+    except ValueError:
+        body = "The webpage for your run of CSET is now ready, though the address could not be determined.\nCheck that WEB_ADDR and WEB_DIR are set correctly, then consider filing a bug report at https://github.com/MetOffice/CSET"
     subprocess.run(
         f'printf "{body}" | mail -s "{subject}" -S "from=notifications" "$USER"',
         check=True,
