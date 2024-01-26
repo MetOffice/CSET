dfbacf4c55f290338f2c9afdc906c3d9fe6425d3
James Frost <james.frost@metoffice.gov.uk>
2024-01-26 09:51:19 +0000

    Use HTML directly for workflow status
    Fixes #375

--- cset-workflow/app/finish_website/bin/finish-website.py
+++ cset-workflow/app/finish_website/bin/finish-website.py
@@ -6,14 +6,7 @@ Does the final update to the workflow status on the front page of the web
 interface.
 """
 
-import fcntl
-import json
 import os
 
-with open(f"{os.getenv('WEB_DIR')}/status.json", "r+t") as fp:
-    fcntl.flock(fp, fcntl.LOCK_EX)
-    status_dict = json.load(fp)
-    status_dict["status"] = "<p>Finished</p>"
-    fp.seek(0)
-    fp.truncate()
-    json.dump(status_dict, fp)
+with open(f"{os.getenv('WEB_DIR')}/status.json", "wt") as fp:
+    fp.write("<p>Finished</p>\n")
