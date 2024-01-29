9ad14092761445fe01f5bdc929c914e625060bfe
James Frost <james.frost@metoffice.gov.uk>
2024-01-29 13:10:33 +0000

    Narrow scope of plot tests so they don't run recipe
    This allows them to be faster, and more effectively pinpoint problems.

--- tests/operators/test_plots.py
+++ tests/operators/test_plots.py
@@ -19,34 +19,7 @@ from pathlib import Path
 import iris.cube
 import pytest
 
-from CSET.operators import execute_recipe, plot
-
-
-def test_spatial_plot(tmp_path: Path):
-    """Plot spatial contour plot of instant air temp."""
-    input_file = Path("tests/test_data/air_temp.nc")
-    output_dir = tmp_path / "output"
-    recipe_file = Path("tests/test_data/plot_instant_air_temp.yaml")
-    execute_recipe(recipe_file, input_file, output_dir)
-    actual_output_file = output_dir / "plot.png"
-    assert actual_output_file.is_file()
-
-
-def test_postage_stamp_plots(tmp_path: Path):
-    """Plot postage stamp plots of ensemble data."""
-    input_file = Path("tests/test_data/")
-    output_dir = tmp_path / "output"
-    recipe_file = Path("tests/test_data/ensemble_air_temp.yaml")
-    execute_recipe(recipe_file, input_file, output_dir)
-    assert output_dir.joinpath("plot.png").is_file()
-
-
-def test_postage_stamp_realization_check(tmp_path: Path, cube):
-    """Check error when cube has no realization coordinate."""
-    cube.remove_coord("realization")
-    plot_path = tmp_path / "plot.png"
-    with pytest.raises(ValueError):
-        plot.postage_stamp_contour_plot(cube, plot_path)
+from CSET.operators import plot, read
 
 
 def test_check_single_cube():
@@ -61,6 +34,31 @@ def test_check_single_cube():
         plot._check_single_cube(long_cubelist)
     with pytest.raises(TypeError):
         plot._check_single_cube(non_cube)
+
+
+def test_spatial_contour_plot(cube, tmp_working_dir):
+    """Plot spatial contour plot of instant air temp."""
+    plot_file = Path("plot.png")
+    cube_2d = cube.slices_over("time").next()
+    plot.spatial_contour_plot(cube_2d, filename=plot_file)
+    assert plot_file.is_file()
+
+
+def test_postage_stamp_plots(monkeypatch, tmp_path):
+    """Plot postage stamp plots of ensemble data."""
+    ensemble_cube = read.read_cube("tests/test_data/exeter_em*.nc")
+    ensemble_cube_3d = ensemble_cube.slices_over("time").next()
+    monkeypatch.chdir(tmp_path)
+    plot_file = Path("plot.png")
+    plot.postage_stamp_contour_plot(ensemble_cube_3d, filename=plot_file)
+    assert plot_file.is_file()
+
+
+def test_postage_stamp_realization_check(cube, tmp_working_dir):
+    """Check error when cube has no realization coordinate."""
+    cube.remove_coord("realization")
+    with pytest.raises(ValueError):
+        plot.postage_stamp_contour_plot(cube)
 
 
 def test_contour_plot_sequence(cube, tmp_working_dir):
