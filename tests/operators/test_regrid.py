ea22610c4d7d0816365cc5b654ed2bdbccae1e7b
jwarner8 <james.warner@metoffice.gov.uk>
2024-02-27 23:24:35 +0000

    specify var to load

--- tests/operators/test_regrid.py
+++ tests/operators/test_regrid.py
@@ -10,7 +10,9 @@ import CSET.operators.regrid as regrid
 def test_regrid_onto_xyspacing():
     """Test regrid case where x and y spacing are specified."""
     # Test 1: Rectilinear GeogCS grid case
-    test_data = iris.load_cube("tests/test_data/regrid/regrid_rectilinearGeogCS.nc")
+    test_data = iris.load_cube(
+        "tests/test_data/regrid/regrid_rectilinearGeogCS.nc", "surface_altitude"
+    )
     regridded_test_data = iris.load_cube(
         "tests/test_data/regrid/out_rectilinearGeogCS_0p5deg.nc"
     )
@@ -26,7 +28,9 @@ def test_regrid_onto_xyspacing():
 def test_regrid_onto_cube():
     """Test regrid case where target cube to project onto is specified."""
     # Test 1: Rectilinear GeogCS grid case
-    test_data = iris.load_cube("tests/test_data/regrid/regrid_rectilinearGeogCS.nc")
+    test_data = iris.load_cube(
+        "tests/test_data/regrid/regrid_rectilinearGeogCS.nc", "surface_altitude"
+    )
     regridded_test_data = iris.load_cube(
         "tests/test_data/regrid/out_rectilinearGeogCS_0p5deg.nc"
     )
