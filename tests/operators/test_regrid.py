42bd1b0acd079b6b456d619e0c92af53fdf10d46
jwarner8 <james.warner@metoffice.gov.uk>
2024-03-22 10:27:46 +0000

    use np.allclose

--- tests/operators/test_regrid.py
+++ tests/operators/test_regrid.py
@@ -3,6 +3,7 @@
 """Tests regrid operator."""
 
 import iris
+import numpy as np
 
 import CSET.operators.regrid as regrid
 
@@ -17,11 +18,13 @@ def test_regrid_onto_xyspacing():
         "tests/test_data/regrid/out_rectilinearGeogCS_0p5deg.nc"
     )
 
-    assert (
+    assert np.allclose(
         regrid.regrid_onto_xyspacing(
             test_data, xspacing=0.5, yspacing=0.5, regridmethod="Linear"
-        ).data.all()
-        == regridded_test_data.data.all()
+        ).data.all(),
+        regridded_test_data.data.all(),
+        rtol=1e-02,
+        atol=1e-02,
     )
 
 
@@ -35,9 +38,11 @@ def test_regrid_onto_cube():
         "tests/test_data/regrid/out_rectilinearGeogCS_0p5deg.nc"
     )
 
-    assert (
+    assert np.allclose(
         regrid.regrid_onto_cube(
             test_data, regridded_test_data, regridmethod="Linear"
-        ).data.all()
-        == regridded_test_data.data.all()
+        ).data.all(),
+        regridded_test_data.data.all(),
+        rtol=1e-02,
+        atol=1e-02,
     )
