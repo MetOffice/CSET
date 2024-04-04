e4f42cb0932740cc45e7892b224e923b3ac38b33
James Frost <james.frost@metoffice.gov.uk>
2024-04-04 15:10:36 +0100

    Test errors for regrid failure cases
    * Missing coordinates,
    * Invalid and nonexistent regridding method,
    * Invalid coordinate systems.

--- tests/operators/test_regrid.py
+++ tests/operators/test_regrid.py
@@ -15,44 +15,148 @@
 """Tests regrid operator."""
 
 import iris
+import iris.coord_systems
+import iris.cube
 import numpy as np
+import pytest
 
 import CSET.operators.regrid as regrid
 
 
-def test_regrid_onto_xyspacing():
-    """Test regrid case where x and y spacing are specified."""
-    # Test 1: Rectilinear GeogCS grid case
-    test_data = iris.load_cube(
+# Session scope fixtures, so the test data only has to be loaded once.
+@pytest.fixture(scope="session")
+def regrid_source_cube() -> iris.cube.Cube:
+    """Get a cube to regrid."""
+    return iris.load_cube(
         "tests/test_data/regrid/regrid_rectilinearGeogCS.nc", "surface_altitude"
     )
-    regridded_test_data = iris.load_cube(
-        "tests/test_data/regrid/out_rectilinearGeogCS_0p5deg.nc"
-    )
-
-    assert np.allclose(
-        regrid.regrid_onto_xyspacing(
-            test_data, xspacing=0.5, yspacing=0.5, method="Linear"
-        ).data,
-        regridded_test_data.data,
-        rtol=1e-06,
-        atol=1e-02,
-    )
 
 
-def test_regrid_onto_cube():
+@pytest.fixture(scope="session")
+def regrid_test_cube() -> iris.cube.Cube:
+    """Get a regridded cube to compare."""
+    return iris.load_cube("tests/test_data/regrid/out_rectilinearGeogCS_0p5deg.nc")
+
+
+def test_regrid_onto_cube(regrid_source_cube, regrid_test_cube):
     """Test regrid case where target cube to project onto is specified."""
     # Test 1: Rectilinear GeogCS grid case
-    test_data = iris.load_cube(
-        "tests/test_data/regrid/regrid_rectilinearGeogCS.nc", "surface_altitude"
-    )
-    regridded_test_data = iris.load_cube(
-        "tests/test_data/regrid/out_rectilinearGeogCS_0p5deg.nc"
-    )
-
     assert np.allclose(
-        regrid.regrid_onto_cube(test_data, regridded_test_data, method="Linear").data,
-        regridded_test_data.data,
+        regrid.regrid_onto_cube(
+            regrid_source_cube, regrid_test_cube, method="Linear"
+        ).data,
+        regrid_test_cube.data,
         rtol=1e-06,
         atol=1e-02,
     )
+
+
+def test_regrid_onto_cube_missing_coord(regrid_source_cube, regrid_test_cube):
+    """Missing coordinate raises error."""
+    # Missing X coordinate.
+    source = regrid_source_cube.copy()
+    source.remove_coord("longitude")
+    with pytest.raises(ValueError):
+        regrid.regrid_onto_cube(source, regrid_test_cube, method="Linear")
+
+    # Missing Y coordinate.
+    source = regrid_source_cube.copy()
+    source.remove_coord("latitude")
+    with pytest.raises(ValueError):
+        regrid.regrid_onto_cube(source, regrid_test_cube, method="Linear")
+
+
+def test_regrid_onto_cube_unknown_crs(regrid_source_cube, regrid_test_cube):
+    """Coordinate reference system is unrecognised."""
+    # Exchange X to unsupported coordinate system.
+    source_changed_x = regrid_source_cube.copy()
+    source_changed_x.coord("longitude").coord_system = iris.coord_systems.OSGB()
+    with pytest.raises(NotImplementedError):
+        regrid.regrid_onto_cube(source_changed_x, regrid_test_cube, method="Linear")
+
+    # Exchange Y to unsupported coordinate system.
+    source_changed_y = regrid_source_cube.copy()
+    source_changed_y.coord("latitude").coord_system = iris.coord_systems.OSGB()
+    with pytest.raises(NotImplementedError):
+        regrid.regrid_onto_cube(source_changed_y, regrid_test_cube, method="Linear")
+
+
+def test_regrid_onto_cube_non_callable_method(regrid_source_cube, regrid_test_cube):
+    """Method exists but is not callable."""
+    with pytest.raises(NotImplementedError):
+        regrid.regrid_onto_cube(regrid_source_cube, regrid_test_cube, method="maths")
+
+
+def test_regrid_onto_cube_unknown_method(regrid_source_cube, regrid_test_cube):
+    """Method does not exist."""
+    with pytest.raises(NotImplementedError):
+        regrid.regrid_onto_cube(
+            regrid_source_cube, regrid_test_cube, method="nonexistent"
+        )
+
+
+def test_regrid_onto_xyspacing(regrid_source_cube, regrid_test_cube):
+    """Test regrid case where x and y spacing are specified."""
+    # Test 1: Rectilinear GeogCS grid case
+    assert np.allclose(
+        regrid.regrid_onto_xyspacing(
+            regrid_source_cube, xspacing=0.5, yspacing=0.5, method="Linear"
+        ).data,
+        regrid_test_cube.data,
+        rtol=1e-06,
+        atol=1e-02,
+    )
+
+
+def test_regrid_onto_xyspacing_missing_coord(regrid_source_cube):
+    """Missing coordinate raises error."""
+    # Missing X coordinate.
+    source = regrid_source_cube.copy()
+    source.remove_coord("longitude")
+    with pytest.raises(ValueError):
+        regrid.regrid_onto_xyspacing(
+            source, xspacing=0.5, yspacing=0.5, method="Linear"
+        )
+
+    # Missing Y coordinate.
+    source = regrid_source_cube.copy()
+    source.remove_coord("latitude")
+    with pytest.raises(ValueError):
+        regrid.regrid_onto_xyspacing(
+            source, xspacing=0.5, yspacing=0.5, method="Linear"
+        )
+
+
+def test_regrid_onto_xyspacing_unknown_crs(regrid_source_cube):
+    """Coordinate reference system is unrecognised."""
+    # Exchange X to unsupported coordinate system.
+    source_changed_x = regrid_source_cube.copy()
+    source_changed_x.coord("longitude").coord_system = iris.coord_systems.OSGB()
+    with pytest.raises(NotImplementedError):
+        regrid.regrid_onto_xyspacing(
+            source_changed_x, xspacing=0.5, yspacing=0.5, method="Linear"
+        )
+
+    # Exchange Y to unsupported coordinate system.
+    source_changed_y = regrid_source_cube.copy()
+    source_changed_y.coord("latitude").coord_system = iris.coord_systems.OSGB()
+    with pytest.raises(NotImplementedError):
+        regrid.regrid_onto_xyspacing(
+            source_changed_y, xspacing=0.5, yspacing=0.5, method="Linear"
+        )
+
+
+def test_regrid_onto_xyspacing_non_callable_method(regrid_source_cube):
+    """Method exists but is not callable."""
+    with pytest.raises(NotImplementedError):
+        regrid.regrid_onto_xyspacing(
+            regrid_source_cube, xspacing=0.5, yspacing=0.5, method="maths"
+        )
+
+
+def test_regrid_onto_xyspacing_unknown_method(regrid_source_cube):
+    """Method does not exist."""
+    with pytest.raises(NotImplementedError):
+        regrid.regrid_onto_xyspacing(
+            regrid_source_cube, xspacing=0.5, yspacing=0.5, method="nonexistent"
+        )
