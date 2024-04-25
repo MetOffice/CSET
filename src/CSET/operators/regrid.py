357f49b0632efc580b16f631fe44809710b2dd10
JKPShonk <jon.shonk@metoffice.gov.uk>
2024-04-25 12:21:38 +0100

    Draft commit with new Operator and Recipe added to allow user to extract data at an individual gridpoint.

--- src/CSET/operators/regrid.py
+++ src/CSET/operators/regrid.py
@@ -179,3 +179,110 @@ def regrid_onto_xyspacing(
         raise NotImplementedError(f"Does not currently support {method} regrid method")
 
     return cube_rgd
+
+
+# ***** We created this new Operator as a modified version of the one above.
+#      We added the "lon_pt" and "lat_pt" as new inputs specifying the chosen point.
+def regrid_to_single_point(
+    incube: iris.cube.Cube, lat_pt: float, lon_pt: float, method: str, **kwargs
+) -> iris.cube.Cube:
+    """Select data at a single point by longitude and latitude.
+
+    Selection is performed by a regrid function, selecting the nearest gridpoint to
+    the selected longitude and latitude values.
+
+    Parameters
+    ----------
+    incube: Cube
+        An iris cube of the data to regrid. As a minimum, it needs to be 2D with a latitude,
+        longitude coordinates.
+    lon_pt: float
+        Selected value of longitude.
+    lat_pt: float
+        Selected value of latitude.
+    method: str
+        Method used to determine the values at the selected longitude and latitude.
+        The recommended approach is to use iris.analysis.Nearest(), which selects the
+        nearest gridpoint. An alternative is iris.analysis.Linear(), which obtains
+        the values at the selected longitude and latitude by linear interpolation.
+
+    Returns
+    -------
+    cube_rgd: Cube
+        An iris cube of the data at the specified point (this will have time
+        and/or height dimensions).
+
+    Raises
+    ------
+    ValueError
+        If a unique x/y coordinate cannot be found; also if, for selecting a single
+        gridpoint, the chosen longitude and latitude point is outside the domain.
+    [TO DO: these limits may need updating to exclude selecting points too near the
+    boundaries.]
+    NotImplementedError
+        If the cubes grid, or the method for regridding, is not yet supported.
+
+    Notes
+    -----
+    The acceptable coordinate names for X and Y coordinates are currently described
+    in X_COORD_NAMES and Y_COORD_NAMES. These cover commonly used coordinate types,
+    though a user can append new ones.
+    Currently rectlinear grids (uniform) are supported.
+
+    """
+    # Get a list of coordinate names for the cube
+    coord_names = [coord.name() for coord in incube.coords()]
+
+    # Check which x-coordinate we have, if any
+    x_coords = [coord for coord in coord_names if coord in X_COORD_NAMES]
+    if len(x_coords) != 1:
+        raise ValueError("Could not identify a unique x-coordinate in cube")
+    x_coord = incube.coord(x_coords[0])
+
+    # Check which y-coordinate we have, if any
+    y_coords = [coord for coord in coord_names if coord in Y_COORD_NAMES]
+    if len(y_coords) != 1:
+        raise ValueError("Could not identify a unique y-coordinate in cube")
+    y_coord = incube.coord(y_coords[0])
+
+    # List of supported grids - check if it is compatible
+    supported_grids = (iris.coord_systems.GeogCS, iris.coord_systems.RotatedGeogCS)
+    # **** We added the RotatedGeogCS grid to the list above
+    #      so that it works with rotated grids.
+    if not isinstance(incube.coord(x_coord).coord_system, supported_grids):
+        raise NotImplementedError(
+            f"Does not currently support {incube.coord(x_coord).coord_system} regrid method"
+        )
+    if not isinstance(incube.coord(y_coord).coord_system, supported_grids):
+        raise NotImplementedError(
+            f"Does not currently support {incube.coord(y_coord).coord_system} regrid method"
+        )
+
+    # Get axis
+    lat, lon = incube.coord(y_coord), incube.coord(x_coord)
+
+    # Get bounds
+    lat_min, lon_min = lat.points.min(), lon.points.min()
+    lat_max, lon_max = lat.points.max(), lon.points.max()
+
+    # Check to see if selected point is outside the domain
+    if (
+        (lat_pt < lat_min)
+        or (lat_pt > lat_max)
+        or (lon_pt < lon_min)
+        or (lon_pt > lon_max)
+    ):
+        raise ValueError("Selected point is outside the domain.")
+    # **** We added these criteria to check that the point is within the domain.
+
+    regrid_method = getattr(iris.analysis, method, None)
+    if callable(regrid_method):
+        cube_rgd = incube.interpolate(
+            [(y_coord, lat_pt), (x_coord, lon_pt)], regrid_method
+        )
+        # **** This is then the updated interpolation method, reflects exactly the same
+        #     as the version above ("regrid_onto_xyspacing").
+    else:
+        raise NotImplementedError(f"Does not currently support {method} regrid method")
+
+    return cube_rgd
