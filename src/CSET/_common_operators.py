8ef17998245b7916347f3556e2909c1d62842db3
jwarner8 <james.warner@metoffice.gov.uk>
2024-05-16 11:38:41 +0100

    tidyuo

--- src/CSET/_common_operators.py
+++ src/CSET/_common_operators.py
@@ -12,4 +12,55 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Common operator functionality used across CSET."""
+"""
+Common operator functionality used across CSET.
+
+Functions below should only be added if it is not suitable as a standalone
+operator, and will be used across multiple operators.
+"""
+
+import iris
+import iris.cube
+
+
+def get_cube_xycoordname(cube: iris.cube.Cube, **kwargs) -> tuple:
+    """
+    Return horizontal coordinate name(s) from a given cube.
+
+    Arguments
+    ---------
+
+    cube: iris.cube.Cube
+        An iris cube which will be checked to see if it contains coordinate
+        names that match a pre-defined list of acceptable horizontal
+        coordinate names.
+
+    Returns
+    -------
+    tuple
+        A tuple containing the horizontal coordinate name for latitude and longitude respectively
+        found within the cube.
+
+    Raises
+    ------
+    ValueError
+        If an x/y horizontal coordinates cannot be found.
+    """
+    # Acceptable horizontal coordinate names.
+    X_COORD_NAMES = ["longitude", "grid_longitude", "projection_x_coordinate", "x"]
+    Y_COORD_NAMES = ["latitude", "grid_latitude", "projection_y_coordinate", "y"]
+
+    # Get a list of coordinate names for the cube
+    coord_names = [coord.name() for coord in cube.coords()]
+
+    # Check which x-coordinate we have, if any
+    x_coords = [coord for coord in coord_names if coord in X_COORD_NAMES]
+    if len(x_coords) != 1:
+        raise ValueError("Could not identify a unique x-coordinate in cube")
+
+    # Check which y-coordinate we have, if any
+    y_coords = [coord for coord in coord_names if coord in Y_COORD_NAMES]
+    if len(y_coords) != 1:
+        raise ValueError("Could not identify a unique y-coordinate in cube")
+
+    return (y_coords[0], x_coords[0])
