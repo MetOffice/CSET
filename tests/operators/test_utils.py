f42ca322098d6bc21a121669f7550f3cd63b6f68
jwarner8 <james.warner@metoffice.gov.uk>
2024-05-17 09:42:15 +0100

    tidyup

--- tests/operators/test_utils.py
+++ tests/operators/test_utils.py
@@ -47,8 +47,7 @@ def test_missing_coord_get_cube_xycoordname(source_cube):
 
 def test_get_cube_xycoordname(source_cube):
     """Check that function returns tuple containing horizontal dimension names."""
-    source = source_cube.copy()
-    assert (common_operators.get_cube_xycoordname(source)) == (
+    assert (common_operators.get_cube_xycoordname(source_cube)) == (
         "latitude",
         "longitude",
     )
