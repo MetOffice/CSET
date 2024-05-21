df6fd6a220a8e707760e2d0008e1137d7a91a317
James Frost <james.frost@metoffice.gov.uk>
2024-05-21 12:46:40 +0100

    Use getopts for nicer argument parsing in install_restricted_files.sh
    getopts is a really nice feature. Thanks Jorge!

--- cset-workflow/install_restricted_files.sh
+++ cset-workflow/install_restricted_files.sh
@@ -3,40 +3,49 @@
 set -euo pipefail
 IFS="$(printf '\n\t')"
 
-if [[ "${1-}" == -h ]] || [[ "${1-}" == --help ]]
-then
+show_help()
+{
   cat << EOF
-Usage: ${0} [<branch-name>] [<repository-url>]
+Usage: ${0} [-b branch-name] [-R repository]
 
-  * If <repository-url> is omitted it defaults to
-    https://github.com/MetOffice/CSET-workflow
-  * If <branch-name> is omitted it defaults to main. It cannot be omitted
-    if you specify <repository-url>, as they are positional arguments.
-  * Most people should omit both, and use the default values.
+  * Repository defaults to https://github.com/MetOffice/CSET-workflow
+  * Branch defaults to main
+  * Most people should omit both, and use the default values
 
 This script downloads and install Momentum® Partnership restricted files and
 code for use in CSET. Make sure you have git authentication setup for the
 repository at https://github.com/MetOffice/CSET-workflow
 EOF
-  exit 0
-fi
+  exit "$1"
+}
 
-# Basic check the we are in the right folder.
+# Set defaults. These are left unchanged if the option isn't specified.
+branch="main"
+httpurl="https://github.com/MetOffice/CSET-workflow"
+
+# Parse options.
+while getopts "hb:R:" opt
+do
+  case "$opt" in
+    b) branch="$OPTARG"
+    ;;
+    R) httpurl="$OPTARG"
+    ;;
+    h) show_help 0
+    ;;
+    ?) show_help 1
+  esac
+done
+
+# Basic check that we are in the right folder.
 if [[ ! -f flow.cylc ]]
 then
   echo "You must be in the cset-workflow directory when running this script."
   exit 1
 fi
 
-if [[ -z "${1-}" ]]
-then
-  branch="main"
-fi
-
-if [[ -z "${2-}" ]]
-then
-  httpurl="https://github.com/MetOffice/CSET-workflow"
-fi
+# Convert GitHub HTTPS url into SSH one.
+# GitLab prepends `ssh.` to its SSH URLs, so is not currently supported.
 sshurl="$(echo "$httpurl" | sed -e 's/https:\/\//git@/' -e 's/\//:/')"
 
 # Make a temporary directory into which to clone the repository.
@@ -44,9 +53,11 @@ tempdir="$(mktemp -d)"
 
 # We don't need history, so shallow git clone for speed.
 # First try with SSH cloning.
+echo "Cloning ${branch} from ${sshurl}"
 if ! git clone --branch "${branch}" --depth 1 "${sshurl}" "${tempdir}"
 then
   echo "Cannot clone via SSH, falling back to HTTPS."
+  echo "Cloning ${branch} from ${httpurl}"
   if ! git clone --branch "${branch}" --depth 1 "${httpurl}" "${tempdir}"
   then
     echo "Problem cloning git repository."
@@ -60,8 +71,16 @@ fi
 rm "${tempdir}/README.md"
 # Ignores top level hidden files.
 cp -rv "${tempdir}"/* "$PWD"
-
-# Clean up, must force here to remove .git folder.
+# Clean up. Must force here to remove .git folder.
 rm -rf "${tempdir}"
 
-echo "Restricted files installed!"
+# Report success.
+cat << EOF
+   ___________ ____________
+  / ____/ ___// ____/_  __/
+ / /    \__ \/ __/   / /
+/ /___ ___/ / /___  / /
+\____//____/_____/ /_/
+
+Restricted files installed!
+EOF
