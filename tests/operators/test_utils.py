89f3d58f0449346d593e17a271d3a0e6d47043c0
jwarner8 <james.warner@metoffice.gov.uk>
2024-07-08 13:32:31 +0100

    add new operator and tests

--- tests/operators/test_utils.py
+++ tests/operators/test_utils.py
@@ -72,7 +72,47 @@ def test_is_transect_noverticalcoord(transect_source_cube):
 
 
 def test_combine_singlecube_into_cubelist(source_cube):
-    """TBD."""
+    """Test case of single cube into cubelist."""
     cubelist = common_operators.combine_cubes_into_cubelist(source_cube)
 
-    print(cubelist)
+    expected_cubelist = (
+        "[<iris 'Cube' of surface_altitude / (m) (latitude: 200; longitude: 200)>]"
+    )
+    assert repr(cubelist) in expected_cubelist
+
+
+def test_combine_single_noncompliant_into_cubelist():
+    """Test case of single object which isn't cube or cubelist."""
+    with pytest.raises(TypeError):
+        common_operators.combine_cubes_into_cubelist("hello")
+
+
+def test_combine_multiplecube_into_cubelist(source_cube, transect_source_cube):
+    """Test case of multiple cube into cubelist."""
+    cubelist = common_operators.combine_cubes_into_cubelist(
+        source_cube, a=transect_source_cube
+    )
+
+    expected_cubelist = "[<iris 'Cube' of surface_altitude / (m) (latitude: 200; longitude: 200)>,\n<iris 'Cube' of air_temperature / (K) (time: 2; pressure: 16; latitude: 6; longitude: 6)>]"
+
+    assert repr(cubelist) in expected_cubelist
+
+
+def test_combine_multiple_cube_and_noncompliant_into_cubelist(source_cube):
+    """Test case of a valid cube with some non compliant data which isn't cube or cubelist."""
+    with pytest.raises(TypeError):
+        common_operators.combine_cubes_into_cubelist(source_cube, a="hello")
+
+
+def test_combine_multiplecube_mixed_into_cubelist(source_cube, transect_source_cube):
+    """Test case of multiple cubes and cubelist into cubelist."""
+    cubelist = common_operators.combine_cubes_into_cubelist(
+        source_cube, a=transect_source_cube
+    )
+    out_cubelist = common_operators.combine_cubes_into_cubelist(
+        source_cube, a=cubelist, b=transect_source_cube
+    )
+
+    expected_cubelist = "[<iris 'Cube' of surface_altitude / (m) (latitude: 200; longitude: 200)>,\n<iris 'Cube' of surface_altitude / (m) (latitude: 200; longitude: 200)>,\n<iris 'Cube' of air_temperature / (K) (time: 2; pressure: 16; latitude: 6; longitude: 6)>,\n<iris 'Cube' of air_temperature / (K) (time: 2; pressure: 16; latitude: 6; longitude: 6)>]"
+
+    assert repr(out_cubelist) in expected_cubelist
