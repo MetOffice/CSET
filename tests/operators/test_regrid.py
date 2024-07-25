3e5ae43b2de3a75bc9a0d48c6ca4232afb7ebb6a
James W <62252918+jwarner8@users.noreply.github.com>
2024-07-25 12:26:29 +0100

    Update tests/operators/test_regrid.py
    Co-authored-by: James Frost <james.frost@metoffice.gov.uk>

--- tests/operators/test_regrid.py
+++ tests/operators/test_regrid.py
@@ -59,7 +59,6 @@ def test_regrid_onto_cube_cubes(regrid_source_cube, regrid_test_cube):
     regridded_cubes = regrid.regrid_onto_cube(
         cubelist_to_regrid, regrid_test_cube, method="Linear"
     )
-
     expected_cubelist = "[<iris 'Cube' of surface_altitude / (m) (latitude: 16; longitude: 16)>,\n<iris 'Cube' of surface_altitude / (m) (latitude: 16; longitude: 16)>]"
     assert repr(regridded_cubes) in expected_cubelist
 
