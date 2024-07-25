a9448a9050a27352515d9d211d0c7cfbe6257b43
James W <62252918+jwarner8@users.noreply.github.com>
2024-07-25 12:26:35 +0100

    Update tests/operators/test_regrid.py
    Co-authored-by: James Frost <james.frost@metoffice.gov.uk>

--- tests/operators/test_regrid.py
+++ tests/operators/test_regrid.py
@@ -55,7 +55,6 @@ def test_regrid_onto_cube_cubes(regrid_source_cube, regrid_test_cube):
     """Test regrid case where target cube to project onto is specified for multiple cubes."""
     # Create cubelist with multiple cubes.
     cubelist_to_regrid = iris.cube.CubeList([regrid_source_cube, regrid_source_cube])
-
     regridded_cubes = regrid.regrid_onto_cube(
         cubelist_to_regrid, regrid_test_cube, method="Linear"
     )
