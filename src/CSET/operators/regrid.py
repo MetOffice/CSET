0bef497fc269bd8b1e52c09a36d250ffe19e3b31
James Frost <james.frost@metoffice.gov.uk>
2024-07-25 11:34:37 +0100

    Update copyright attribution per legal advice
    This now matches that suggested by Met Office legal.
    
    Fixes #751

--- src/CSET/operators/regrid.py
+++ src/CSET/operators/regrid.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Met Office and contributors.
+# © Crown copyright, Met Office (2022-2024) and CSET contributors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
