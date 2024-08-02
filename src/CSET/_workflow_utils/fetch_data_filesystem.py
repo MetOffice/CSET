4c008a76ef0359eaee5cfbbaa5107fe5e869cce0
James Frost <james.frost@metoffice.gov.uk>
2024-08-02 12:02:45 +0100

    Change how data is fetched to work on data time
    Also now use subclasses for getting individual files from the various
    data sources.

--- src/CSET/_workflow_utils/fetch_data_filesystem.py
+++ src/CSET/_workflow_utils/fetch_data_filesystem.py
@@ -6,12 +6,42 @@ import glob
 import logging
 import os
 import shutil
+from datetime import datetime
+
+import isodate
 
 from CSET._workflow_utils import validity_time_tester
 
 logging.basicConfig(
     level=os.getenv("LOGLEVEL", "INFO"), format="%(asctime)s %(levelname)s %(message)s"
 )
+
+# os.getenv("DATA_SOURCE")
+
+
+def template_file_path():
+    """Fill time placeholders to generate a file path to fetch."""
+    raw_path = os.environ["DATA_PATH"]
+    date_type = os.environ["DATE_TYPE"]
+    data_period = isodate.parse_duration(os.getenv("DATA_PERIOD"))
+    data_time = datetime.fromisoformat(os.environ["CYLC_TASK_CYCLE_POINT"])
+    forecast_length = isodate.parse_duration(os.environ["CSET_VERIFICATION_PERIOD"])
+    forecast_offset = isodate.parse_duration(os.environ["CSET_VERIFICATION_OFFSET"])
+
+    placeholder_times = set()
+    match date_type:
+        case "validity":
+            date = data_time
+            while date < data_time + forecast_length:
+                placeholder_times.add(date)
+                date += data_period
+        case "initiation":
+            placeholder_times.add(data_time)
+        case "lead":
+            # TODO: Figure out how we are doing lead time.
+            pass
+        case _:
+            raise ValueError(f"Invalid date type: {date_type}")
 
 
 # Excluded from coverage temporarily as script has be rewritten when data time
