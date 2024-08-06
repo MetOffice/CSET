0717f5674288db12c09ccf58d9bd8a46e4e08e72
James Frost <james.frost@metoffice.gov.uk>
2024-08-06 17:44:01 +0100

    Add workflow exported functions to __all__

--- cset-workflow/lib/python/jinja_utils.py
+++ cset-workflow/lib/python/jinja_utils.py
@@ -1,8 +1,20 @@
 """Useful functions for the workflow."""
 
-# Reexport some functions for use within workflow.
-from builtins import zip  # noqa: F401
-from glob import glob  # noqa: F401
+import itertools
+from builtins import max, min, zip
+from glob import glob
+
+# Reexport functions for use within workflow.
+__all__ = [
+    "get_models",
+    "restructure_field_list",
+    "sanitise_task_name",
+    # Reexported functions.
+    "max",
+    "min",
+    "zip",
+    "glob",
+]
 
 
 def get_models(rose_variables: dict):
@@ -23,3 +35,66 @@ def get_models(rose_variables: dict):
             model_vars["number"] = model
             models.append(model_vars)
     return models
+
+
+def _batched(iterable, n):
+    """Implement itertools.batched for Python < 3.12.
+
+    batched('ABCDEFG', 3) → ABC DEF G
+    https://docs.python.org/3/library/itertools.html#itertools.batched
+    """
+    if n < 1:
+        raise ValueError("n must be at least one")
+    iterator = iter(iterable)
+    while batch := tuple(itertools.islice(iterator, n)):
+        yield batch
+
+
+def restructure_field_list(fields: list):
+    """Restructure a 1D list of fields into a 2D list."""
+    # ('m01s03i236', 'temp_at_screen_level', '', '', '', '', '', '', '', '',
+    #  'm01s03i230', 'wind_speed_at_10m', '', '', '', '', '', '', '', '')
+    # -> [{1: "m01s03i236", 2: "temp_at_screen_level"},
+    #     {1: "m01s03i230", 2: "wind_speed_at_10m"}]
+    max_number_of_models = 10
+    assert len(fields) % max_number_of_models == 0
+    # itertools.batched is from python 3.12
+    batched = getattr(itertools, "batched", _batched)
+    all_fields = batched(fields, max_number_of_models)
+    rearranged = [
+        {
+            field[0] + 1: field[1]
+            for field in enumerate(equivalent_model_fields)
+            if field[1]
+        }
+        for equivalent_model_fields in all_fields
+    ]
+    return rearranged
+
+
+def sanitise_task_name(s: str):
+    """Sanitise a string to be used as a Cylc task name.
+
+    Rules per
+    https://cylc.github.io/cylc-doc/stable/html/user-guide/writing-workflows/runtime.html#cylc.flow.unicode_rules.TaskNameValidator
+    The rules for valid task and family names:
+        * must start with: alphanumeric
+        * can only contain: alphanumeric, _, -, +, %, @
+        * cannot start with: _cylc
+        * cannot be: root
+
+    Note that actually there are a few more characters supported, see:
+    https://github.com/cylc/cylc-flow/issues/6288
+    """
+    # Ensure the first character is alphanumeric.
+    if not s[0].isalnum():
+        s = f"sanitised_{s}"
+    # Replace invalid characters with underscores.
+    s = "".join(c if c.isalnum() or c in "-+%@" else "_" for c in s)
+    # Ensure the name is not a reserved name.
+    if s.lower() == "root":
+        s = f"sanitised_{s}"
+    # Ensure the name does not start with "_cylc".
+    if s.lower().startswith("_cylc"):
+        s = f"sanitised_{s}"
+    return s
