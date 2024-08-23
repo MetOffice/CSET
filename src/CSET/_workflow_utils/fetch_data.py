408fd4eeebf84c77134410dbaed5a5268b067116
James Frost <james.frost@metoffice.gov.uk>
2024-08-23 15:55:57 +0100

    Add fetch_data tests

--- src/CSET/_workflow_utils/fetch_data.py
+++ src/CSET/_workflow_utils/fetch_data.py
@@ -9,6 +9,7 @@ import os
 import shutil
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime, timedelta
+from typing import Literal
 
 import isodate
 
@@ -39,7 +40,7 @@ class FileRetriever(abc.ABC):
         logging.debug("Tearing down FileRetriever.")
 
     @abc.abstractmethod
-    def get_file(self, file_path: str, output_dir: str) -> None:
+    def get_file(self, file_path: str, output_dir: str) -> None:  # pragma: no cover
         """Save a file from the data source to the output directory.
 
         Not all of the given paths will exist, so FileNotFoundErrors should be
@@ -82,20 +83,38 @@ class FilesystemFileRetriever(FileRetriever):
                 logging.warning("Failed to copy %s, error: %s", file, err)
 
 
-def _template_file_path():
-    """Fill time placeholders to generate a file path to fetch."""
-    raw_path = os.environ["DATA_PATH"]
-    date_type = os.environ["DATE_TYPE"]
-    data_time = datetime.fromisoformat(os.environ["CYLC_TASK_CYCLE_POINT"])
-    forecast_length = isodate.parse_duration(os.environ["CSET_ANALYSIS_PERIOD"])
-    forecast_offset = isodate.parse_duration(os.environ["CSET_ANALYSIS_OFFSET"])
+def _get_needed_environment_variables() -> dict:
+    """Load the needed variables from the environment."""
+    variables = {
+        "raw_path": os.environ["DATA_PATH"],
+        "date_type": os.environ["DATE_TYPE"],
+        "data_time": datetime.fromisoformat(os.environ["CYLC_TASK_CYCLE_POINT"]),
+        "forecast_length": isodate.parse_duration(os.environ["CSET_ANALYSIS_PERIOD"]),
+        "forecast_offset": isodate.parse_duration(os.environ["CSET_ANALYSIS_OFFSET"]),
+        "share_dir": os.environ["CYLC_WORKFLOW_SHARE_DIR"],
+        "cycle_point": os.environ["CYLC_TASK_CYCLE_POINT"],
+        "model_number": os.environ["MODEL_NUMBER"],
+    }
+    # Data period is not needed for initiation time.
+    if variables["date_type"] != "initiation":
+        variables["data_period"] = isodate.parse_duration(os.environ["DATA_PERIOD"])
+    return variables
 
+
+def _template_file_path(
+    raw_path: str,
+    date_type: Literal["validity", "initiation", "lead"],
+    data_time: datetime,
+    forecast_length: timedelta,
+    forecast_offset: timedelta,
+    data_period: timedelta,
+) -> list[str]:
+    """Fill time placeholders to generate a file path to fetch."""
     placeholder_times: list[datetime] = []
     lead_times: list[timedelta] = []
     match date_type:
         case "validity":
             date = data_time
-            data_period = isodate.parse_duration(os.getenv("DATA_PERIOD"))
             while date < data_time + forecast_length:
                 placeholder_times.append(date)
                 date += data_period
@@ -103,7 +122,6 @@ def _template_file_path():
             placeholder_times.append(data_time)
         case "lead":
             placeholder_times.append(data_time)
-            data_period = isodate.parse_duration(os.getenv("DATA_PERIOD"))
             lead_time = forecast_offset
             while lead_time < forecast_length:
                 lead_times.append(lead_time)
@@ -115,43 +133,54 @@ def _template_file_path():
     for placeholder_time in placeholder_times:
         # Expand out all other format strings.
         path = placeholder_time.strftime(os.path.expandvars(raw_path))
-
-        # Expand out lead time format strings, %N.
-        for lead_time in lead_times:
-            # BUG: Will not respect escaped % signs, e.g: %%N.
-            paths.append(
-                path.replace("%N", f"{int(lead_time.total_seconds()) // 3600:03d}")
-            )
+        if lead_times:
+            # Expand out lead time format strings, %N.
+            for lead_time in lead_times:
+                # BUG: Will not respect escaped % signs, e.g: %%N.
+                paths.append(
+                    path.replace("%N", f"{int(lead_time.total_seconds()) // 3600:03d}")
+                )
         else:
             paths.append(path)
     return paths
 
 
 def fetch_data(file_retriever: FileRetriever = FilesystemFileRetriever):
-    """Fetch the model's data.
+    """Fetch the data for a model.
 
     The following environment variables need to be set:
-     * CSET_ANALYSIS_OFFSET
-     * CSET_ANALYSIS_PERIOD
-     * CYLC_TASK_CYCLE_POINT
-     * DATA_PATH
-     * DATA_PERIOD - If DATE_TYPE is not 'initialisation'
-     * DATE_TYPE
-     * MODEL_NUMBER
+    * CSET_ANALYSIS_OFFSET
+    * CSET_ANALYSIS_PERIOD
+    * CYLC_TASK_CYCLE_POINT
+    * CYLC_WORKFLOW_SHARE_DIR
+    * DATA_PATH
+    * DATA_PERIOD
+    * DATE_TYPE
+    * MODEL_NUMBER
 
     Parameters
     ----------
     file_retriever: FileRetriever
         FileRetriever implementation to use. Defaults to FilesystemFileRetriever.
     """
+    v = _get_needed_environment_variables()
+
     # Prepare output directory.
-    model_number = os.getenv("MODEL_NUMBER")
-    cycle_share_data_dir = f"{os.getenv('CYLC_WORKFLOW_SHARE_DIR')}/cycle/{os.getenv('CYLC_TASK_CYCLE_POINT')}/data/{model_number}"
+    cycle_share_data_dir = (
+        f"{v['share_dir']}/cycle/{v['cycle_point']}/data/{v['model_number']}"
+    )
     os.makedirs(cycle_share_data_dir, exist_ok=True)
     logging.debug("Output directory: %s", cycle_share_data_dir)
 
     # Get file paths.
-    paths = _template_file_path()
+    paths = _template_file_path(
+        v["raw_path"],
+        v["date_type"],
+        v["data_time"],
+        v["forecast_length"],
+        v["forecast_offset"],
+        v["data_period"],
+    )
     logging.info("Retrieving paths:\n%s", "\n".join(paths))
 
     # Use file retriever to transfer data with multiple threads.
