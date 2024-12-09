9f258a6fef614e6f0c0bcb7bec0c92b981b2557a
James W <62252918+jwarner8@users.noreply.github.com>
2024-12-09 13:44:56 +0000

    Update tests/operators/test_ageofair.py
    Co-authored-by: James Frost <james.frost@metoffice.gov.uk>

--- tests/operators/test_ageofair.py
+++ tests/operators/test_ageofair.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests age of air operators."""
+"""Tests for age of air operator."""
 
 import iris
 import iris.cube
