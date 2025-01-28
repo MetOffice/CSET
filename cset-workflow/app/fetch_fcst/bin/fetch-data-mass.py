9de69df3a8ee0b9fef1e7d530dc61d78611e3baa
James Frost <james.frost@metoffice.gov.uk>
2025-01-28 16:13:47 +0000

    Implement required return value for MASSFileRetriever

--- cset-workflow/app/fetch_fcst/bin/fetch-data-mass.py
+++ cset-workflow/app/fetch_fcst/bin/fetch-data-mass.py
@@ -2,7 +2,9 @@
 
 """Retrieve files from MASS."""
 
+import logging
 import subprocess
+import sys
 
 from CSET._workflow_utils.fetch_data import FileRetrieverABC, fetch_data
 
@@ -20,8 +22,24 @@ class MASSFileRetriever(FileRetrieverABC):
             like globs, which will be expanded in a system specific manner.
         output_dir: str
             Path to filesystem directory into which the file should be copied.
+
+        Returns
+        -------
+        bool:
+            True if files were transferred, otherwise False.
         """
-        subprocess.run(["moo", "get", file_path, output_dir])
+        moo_command = ["moo", "get", file_path, output_dir]
+        logging.debug(f"Fetching from MASS with:\n{' '.join(moo_command)}")
+        p = subprocess.run(moo_command)
+        if p.returncode > 0:
+            logging.info(
+                "moo get exited with non-zero code %s.\nstdout: %s\nstderr: %s",
+                p.returncode,
+                p.stdout.decode(sys.stdout.encoding),
+                p.stderr.decode(sys.stderr.encoding),
+            )
+            return False
+        return True
 
 
 fetch_data(MASSFileRetriever)
