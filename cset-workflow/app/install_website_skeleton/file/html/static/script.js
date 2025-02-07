b6e99c8d4d0731e091a145f748860bed8d5f6b17
James Frost <james.frost@metoffice.gov.uk>
2025-02-07 17:05:16 +0000

    Switch web viewer to sidebar layout
    This switches to using a sidebar layout to allow switching plots without
    obstructing the current one. The number of screens has also been reduced
    down to one by adding the capability to select both single and
    side-by-side plots from the same selector.
    
    Fixes #795
    Fixes #302
    Fixes #1129
    Fixes #1130
    Fixes #1131

--- cset-workflow/app/install_website_skeleton/file/html/static/script.js
+++ cset-workflow/app/install_website_skeleton/file/html/static/script.js
@@ -1,116 +1,138 @@
 // JavaScript code that is used by the pages. Plots should not rely on this
 // file, as it will not be stable.
 
-// Display workflow status on index page.
-function update_workflow_status() {
-  const workflow_status = document.getElementById("workflow_status");
-  // Skip if there is no workflow status on page.
-  if (!workflow_status) {
-    return;
+// Toggle display of the extended description for plots. Global variable so it
+// can be referenced at plot insertion time.
+let description_shown = true;
+
+function enforce_description_toggle() {
+  const description_toggle_button = document.getElementById("description-toggle");
+  if (description_shown) {
+    description_toggle_button.textContent = "⇲ Hide description";
+  } else {
+    description_toggle_button.textContent = "⇱ Show description";
   }
-  fetch("status.html")
-    .then((response) => {
-      if (!response.ok) {
-        const message = `There was a problem fetching the index. Status Code: ${response.status}`;
-        console.warn(message);
-        window.alert(message);
-        return;
-      }
-      response.text().then((html) => {
-        if (window.Sanitizer) {
-          // Sanitizer API is supported
-          workflow_status.setHTML(html);
-        } else {
-          // Fallback where it isn't
-          workflow_status.innerHTML = html;
-        }
-      });
-    })
-    .catch((err) => {
-      // Catch non-HTTP fetch errors.
-      console.error("Workflow status could not be retrieved: ", err);
-    });
+  label: for (plot_frame of document.querySelectorAll("iframe")) {
+    const description_container = plot_frame.contentDocument.getElementById(
+      "description-container"
+    );
+    // Skip doing anything if plot not loaded.
+    if (!description_container) {
+      continue label;
+    }
+    // Hide the description if it is exists and is shown, and show if hidden.
+    // Explicitly add and remove rather than toggle class to prevent the plots
+    // getting out of sync.
+    if (description_shown) {
+      description_container.classList.remove("hidden");
+    } else {
+      description_container.classList.add("hidden");
+    }
+  }
 }
 
-// Toggle display of the extended description for plots.
+// Hook up button.
 function setup_description_toggle_button() {
   const description_toggle_button = document.getElementById("description-toggle");
-  // Skip if there is no description on page.
+  // Skip if there is no description toggle on page.
   if (!description_toggle_button) {
     return;
   }
-  let description_shown = true;
-
-  function toggle_description() {
-    const description_container_1 = document
-      .getElementById("plot_frame_1")
-      .contentDocument.getElementById("description-container");
-
-    // Ensure second frame exists before trying to use it.
-    let description_container_2 = null;
-    if (document.getElementById("plot_frame_2")) {
-      description_container_2 = document
-        .getElementById("plot_frame_2")
-        .contentDocument.getElementById("description-container");
-    }
-
-    // Hide the description if it is exists and is shown, and show if hidden.
-    // Explicitly add and remove rather than toggle class to prevent the two
-    // plots getting out of sync.
-    if (description_shown) {
-      description_shown = false;
-      description_toggle_button.textContent = "⇱ Toggle Description";
-      if (description_container_1) {
-        description_container_1.classList.add("hidden");
-      }
-      if (description_container_2) {
-        description_container_2.classList.add("hidden");
-      }
-    } else {
-      description_shown = true;
-      description_toggle_button.textContent = "⇲ Toggle Description";
-      if (description_container_1) {
-        description_container_1.classList.remove("hidden");
-      }
-      if (description_container_2) {
-        description_container_2.classList.remove("hidden");
-      }
-    }
+  description_toggle_button.addEventListener("click", () => {
+    description_shown = !description_shown;
+    enforce_description_toggle();
+  });
+  // Ensure the description toggle persists across changing the frame content.
+  for (const plot_frame of document.querySelectorAll("iframe")) {
+    plot_frame.addEventListener("load", () => {
+      enforce_description_toggle();
+    });
   }
-
-  description_toggle_button.addEventListener("click", toggle_description);
 }
 
-// Plot selection dropdown menus.
-function setup_plots_dropdown() {
-  const dropdown_background = document.querySelectorAll(".dropdown > .background");
-  // Skip if there is no dropdown on page.
-  if (dropdown_background.length === 0) {
+// Display a single plot frame on the page.
+function ensure_single_frame() {
+  const single_frame = document.getElementById("single-frame");
+  const dual_frame = document.getElementById("dual-frame");
+  dual_frame.classList.add("hidden");
+  single_frame.classList.remove("hidden");
+}
+
+// Display two side-by-side plot frames on the page.
+function ensure_dual_frame() {
+  const single_frame = document.getElementById("single-frame");
+  const dual_frame = document.getElementById("dual-frame");
+  single_frame.classList.add("hidden");
+  dual_frame.classList.remove("hidden");
+}
+
+function construct_sidebar_from_data(data) {
+  const sidebar = document.getElementById("plot-selector");
+  // Button icons.
+  const icons = { left: "◧", full: "▣", right: "◨" };
+
+  for (category in data) {
+    // Details element for category.
+    const details = document.createElement("details");
+    details.open = true;
+
+    // Title for category (summary element).
+    const summary = document.createElement("summary");
+    summary.textContent = category;
+    details.append(summary);
+
+    // Menu of plots for this category.
+    const category_menu = document.createElement("menu");
+
+    // Add each plot.
+    for (const plot in data[category]) {
+      // Menu entry for plot.
+      const list_item = document.createElement("li");
+      list_item.textContent = data[category][plot];
+
+      // Container element for plot position chooser buttons.
+      const position_chooser = document.createElement("div");
+      position_chooser.classList.add("plot-position-chooser");
+
+      // Add buttons for each position.
+      for (const position of ["left", "full", "right"]) {
+        // Create button.
+        const button = document.createElement("button");
+        button.classList.add(position);
+        button.textContent = icons[position];
+
+        // Add a callback updating the iframe when the link is clicked.
+        button.addEventListener("click", (event) => {
+          event.preventDefault();
+          // Set the appropriate frame layout.
+          position == "full" ? ensure_single_frame() : ensure_dual_frame();
+          document.getElementById(`plot-frame-${position}`).src = `plots/${plot}`;
+        });
+
+        // Add button to chooser.
+        position_chooser.append(button);
+      }
+
+      // Add position chooser to entry.
+      list_item.append(position_chooser);
+
+      // Add entry to the menu.
+      category_menu.append(list_item);
+    }
+
+    // Join category to the DOM.
+    details.append(category_menu);
+    sidebar.append(details);
+  }
+}
+
+// Plot selection sidebar
+function setup_plots_sidebar() {
+  // Skip if there is no sidebar on page.
+  if (!document.getElementById("plot-selector")) {
     return;
   }
-
-  // Closing of dropdown.
-  function close_dropdowns(event) {
-    if (event instanceof KeyboardEvent) {
-      if (event.key !== "Escape") {
-        return;
-      }
-    }
-    for (const elem of document.querySelectorAll(".dropdown")) {
-      elem.removeAttribute("open");
-    }
-  }
-
-  // Register event listeners.
-  for (const elem of dropdown_background) {
-    elem.addEventListener("click", close_dropdowns);
-  }
-  document.addEventListener("keydown", close_dropdowns);
-  for (const elem of document.querySelectorAll(".dropdown-close")) {
-    elem.addEventListener("click", close_dropdowns);
-  }
-
-  // Loading of plot index file, and adding them to dropdown.
+  // Loading of plot index file, and adding them to the sidebar.
   fetch("plots/index.json")
     .then((response) => {
       // Display a message and stop if the fetch fails.
@@ -120,70 +142,7 @@ function setup_plots_dropdown() {
         window.alert(message);
         return;
       }
-      response.json().then((data) => {
-        // Number of the dropdown within the page.
-        let dropdown_number = 1;
-        // Loop through all the dropdowns and populate them.
-        const dropdowns = document.querySelectorAll(
-          ".dropdown > .dropdown-content > .tabset > .tab-panels"
-        );
-        for (const dropdown_tab_content of dropdowns) {
-          // Number of the tab within the dropdown.
-          let tab_number = 0;
-          for (category in data) {
-            // Control for tab.
-            const input_element = document.createElement("input");
-            input_element.type = "radio";
-            input_element.name = `tabset_${dropdown_number}`;
-            input_element.id = `tab_${dropdown_number}_${tab_number}`;
-            input_element.ariaControls = `panel_${dropdown_number}_${tab_number}`;
-            dropdown_tab_content.insertAdjacentElement("beforebegin", input_element);
-
-            // Label for tab
-            const label_element = document.createElement("label");
-            label_element.htmlFor = `tab_${dropdown_number}_${tab_number}`;
-            label_element.textContent = category;
-            dropdown_tab_content.insertAdjacentElement("beforebegin", label_element);
-
-            // Tab panel with links to plots
-            const tab_section = document.createElement("section");
-            tab_section.id = `panel_${dropdown_number}_${tab_number}`;
-            tab_section.className = "tab-panel";
-
-            // Menu of links.
-            const tab_section_menu = document.createElement("menu");
-            tab_section_menu.className = "plot-selector";
-            // Populate with links.
-            for (plot in data[category]) {
-              // Create a link to the plot.
-              const link = document.createElement("a");
-              link.href = `plots/${plot}`;
-              link.textContent = data[category][plot];
-              // Add a callback updating the iframe when the link is clicked.
-              const frame_id = `plot_frame_${dropdown_number}`;
-              link.addEventListener("click", (event) => {
-                event.preventDefault();
-                document.getElementById(frame_id).src = link.href;
-              });
-              // Add the link to the menu (a list).
-              const list_item = document.createElement("li");
-              list_item.append(link);
-              tab_section_menu.append(list_item);
-            }
-            // Join everything to the DOM.
-            tab_section.append(tab_section_menu);
-            dropdown_tab_content.append(tab_section);
-
-            // On to the next tab.
-            tab_number++;
-          }
-          // Select first tab.
-          dropdown_tab_content.parentNode.querySelector("input").checked = true;
-
-          // On to the next dropdown.
-          dropdown_number++;
-        }
-      });
+      response.json().then(construct_sidebar_from_data);
     })
     .catch((err) => {
       // Catch non-HTTP fetch errors.
@@ -191,7 +150,20 @@ function setup_plots_dropdown() {
     });
 }
 
-// Actually run everything.
-update_workflow_status();
+function setup_clear_view_button() {
+  // Reset frames to placeholder view.
+  function clear_frames() {
+    for (plot_frame of document.querySelectorAll("iframe")) {
+      plot_frame.src = "placeholder.html";
+    }
+    ensure_single_frame();
+  }
+
+  const clear_view_button = document.getElementById("clear-plots");
+  clear_view_button.addEventListener("click", clear_frames);
+}
+
+// Run everything.
 setup_description_toggle_button();
-setup_plots_dropdown();
+setup_clear_view_button();
+setup_plots_sidebar();
