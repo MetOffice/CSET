c5d4d7b8722cfd6d29c4ea51c5566648e531371a
James Frost <james.frost@metoffice.gov.uk>
2025-03-14 17:31:56 +0000

    Conditionally fix times to avoid double fixing

--- src/CSET/operators/read.py
+++ src/CSET/operators/read.py
@@ -455,6 +455,10 @@ def _fix_um_radtime_posthour(cube: iris.cube.Cube):
             time_unit = time_coord.units
             time_points = time_unit.num2date(time_coord.points)
 
+            # Skip if times don't need fixing.
+            if time_points[0].minute != 1:
+                return
+
             # Subtract 1 minute from each time point
             new_time_points = np.array(
                 [t - datetime.timedelta(minutes=1) for t in time_points]
@@ -478,6 +482,10 @@ def _fix_um_radtime_prehour(cube: iris.cube.Cube):
             # Convert time points to datetime objects
             time_unit = time_coord.units
             time_points = time_unit.num2date(time_coord.points)
+
+            # Skip if times don't need fixing.
+            if time_points[0].minute != 59:
+                return
 
             # Add 1 minute from each time point
             new_time_points = np.array(
@@ -513,6 +521,7 @@ def _fix_um_lightning(cube: iris.cube.Cube):
     """
     try:
         if cube.attributes["STASH"] == "m01s21i104":
+            # Remove aggregation cell method.
             cube.cell_methods = []
 
             time_coord = cube.coord("time")
@@ -520,6 +529,10 @@ def _fix_um_lightning(cube: iris.cube.Cube):
             # Convert time points to datetime objects
             time_unit = time_coord.units
             time_points = time_unit.num2date(time_coord.points)
+
+            # Skip if times don't need fixing.
+            if time_points[0].minute == 0:
+                return
 
             # Subtract 1 minute from each time point
             new_time_points = np.array(
