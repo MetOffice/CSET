d47ed830e40e71eaa8c872c082ef7d33d012ea63
James Warner <james.warner@metoffice.gov.uk>
2025-03-17 13:51:59 +0000

    threading through vars

--- src/CSET/operators/read.py
+++ src/CSET/operators/read.py
@@ -39,6 +39,9 @@ class NoDataWarning(UserWarning):
 def read_cube(
     loadpath: list[str] | str,
     constraint: iris.Constraint = None,
+    select_subarea: bool = False,
+    subarea_type:  str = 'RealWorld',
+    subarea_extent: list = [0,0,0,0],
     filename_pattern: str = "*",
     **kwargs,
 ) -> iris.cube.Cube:
@@ -80,7 +83,7 @@ def read_cube(
     ValueError
         If the constraint doesn't produce a single cube.
     """
-    cubes = read_cubes(loadpath, constraint, filename_pattern)
+    cubes = read_cubes(loadpath, constraint, select_subarea,subarea_type,subarea_extent, filename_pattern)
     # Check filtered cubes is a CubeList containing one cube.
     if len(cubes) == 1:
         return cubes[0]
@@ -95,6 +98,9 @@ def read_cube(
 def read_cubes(
     loadpath: list[str] | str,
     constraint: iris.Constraint = None,
+    select_subarea: bool = False,
+    subarea_type:  str = 'RealWorld',
+    subarea_extent: list = [0,0,0,0],
     filename_pattern: str = "*",
     **kwargs,
 ) -> iris.cube.CubeList:
@@ -186,6 +192,13 @@ def read_cubes(
             # Iris can't guess the bounds of a scalar coordinate.
             if not dim_coord.has_bounds() and dim_coord.shape[0] > 1:
                 dim_coord.guess_bounds()
+
+    # To write up
+    if select_subarea:
+        #Some logic here about using appropriate coordinates
+        for cube in cubes:
+            cube = cube.intersection(grid_latitude=(subarea_extent[0],subarea_extent[1]),
+                                     grid_longitude=(subarea_extent[2],subarea_extent[3]))
 
     logging.debug("Loaded cubes: %s", cubes)
     if len(cubes) == 0:
