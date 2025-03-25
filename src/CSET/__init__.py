da09568662c5a7506647bb3cd892e1a1b586b26d
James Frost <james.frost@metoffice.gov.uk>
2025-03-25 15:45:05 +0000

    Add option to skip writing processed data
    A command line option and rose edit setting are added to control the
    saving of processed data.
    
    This can be used to avoid writing the output for large runs, where the
    processed data might take up many terabytes and significantly slow
    processing.
    
    I also sneaked in a minor improvement to the command logging in
    run_cset_recipe, so you can just take the logged command and run it
    directly, without having to worry about quoting things.
    
    Fixes #1271

--- src/CSET/__init__.py
+++ src/CSET/__init__.py
@@ -115,6 +115,9 @@ def setup_argument_parser() -> argparse.ArgumentParser:
     parser_bake.add_argument(
         "--plot-resolution", type=int, help="plotting resolution in dpi"
     )
+    parser_bake.add_argument(
+        "--skip-write", action="store_true", help="Skip saving processed output"
+    )
     parser_bake.set_defaults(func=_bake_command)
 
     parser_graph = subparsers.add_parser("graph", help="visualise a recipe file")
@@ -238,6 +241,7 @@ def _bake_command(args, unparsed_args):
         recipe_variables,
         args.style_file,
         args.plot_resolution,
+        args.skip_write,
     )
 
 
