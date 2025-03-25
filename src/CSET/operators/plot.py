048ce1eba3f90933838947d7d23e1361a7ecbde5
Sylviabohnenstengel <Sylvia.bohnenstengel@metoffice.gov.uk>
2025-03-25 12:31:53 +0000

    move function into private section

--- src/CSET/operators/plot.py
+++ src/CSET/operators/plot.py
@@ -935,6 +935,27 @@ def _spatial_plot(
     _make_plot_html_page(complete_plot_index)
 
 
+def _convert_precipitation_units_callback(cube: iris.cube.Cube):
+    """To convert the unit of precipitation from kg m-2 s-1 to mm hr-1.
+
+    Some precipitation diagnostics are output with unit kg m-2 s-1 and are converted to mm hr-1.
+    """
+    # if cube.attributes["STASH"] == "m01s04i203" or cube.long_name == "surface_microphysical_rainfall_rate":
+    if cube.long_name == "surface_microphysical_rainfall_rate":
+        if cube.units == "kg m-2 s-1":
+            logging.info("Converting precipitation units from kg m-2 s-1 to mm hr-1")
+            # manually convert from kg m-2 s-1 to mm hr-1 assuming 1kg water = 1l water = 1dm^3 water
+            cube.data = cube.data * 3600.0
+
+            # update the units
+            cube.units = "mm hr-1"
+        else:
+            logging.warning(
+                "Precipitation units are not in 'kg m-2 s-1', skipping conversion"
+            )
+    return cube
+
+
 ####################
 # Public functions #
 ####################
@@ -1488,24 +1509,3 @@ def plot_histogram_series(
     _make_plot_html_page(complete_plot_index)
 
     return cubes
-
-
-def _convert_precipitation_units_callback(cube: iris.cube.Cube):
-    """To convert the unit of precipitation from kg m-2 s-1 to mm hr-1.
-
-    Some precipitation diagnostics are output with unit kg m-2 s-1 and are converted to mm hr-1.
-    """
-    # if cube.attributes["STASH"] == "m01s04i203" or cube.long_name == "surface_microphysical_rainfall_rate":
-    if cube.long_name == "surface_microphysical_rainfall_rate":
-        if cube.units == "kg m-2 s-1":
-            logging.info("Converting precipitation units from kg m-2 s-1 to mm hr-1")
-            # manually convert from kg m-2 s-1 to mm hr-1 assuming 1kg water = 1l water = 1dm^3 water
-            cube.data = cube.data * 3600.0
-
-            # update the units
-            cube.units = "mm hr-1"
-        else:
-            logging.warning(
-                "Precipitation units are not in 'kg m-2 s-1', skipping conversion"
-            )
-    return cube
