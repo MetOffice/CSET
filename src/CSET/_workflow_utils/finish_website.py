cbb42ee2b17253a249a8b799124db2ce0dedae2b
ukmo-huw-lewis <48992503+ukmo-huw-lewis@users.noreply.github.com>
2025-05-30 16:16:45 +0100

    Update src/CSET/_workflow_utils/finish_website.py
    Co-authored-by: James Frost <james.frost@metoffice.gov.uk>

--- src/CSET/_workflow_utils/finish_website.py
+++ src/CSET/_workflow_utils/finish_website.py
@@ -68,7 +68,7 @@ def copy_rose_config():
     """Copy the rose-suite.conf file to add to output web directory."""
     rose_suite_conf = Path(os.environ["CYLC_WORKFLOW_RUN_DIR"]) / "rose-suite.conf"
     web_conf_file = (
-        Path(os.environ["CYLC_WORKFLOW_SHARE_DIR"]) / "web" / "rose-suite.conf.txt"
+        Path(os.environ["CYLC_WORKFLOW_SHARE_DIR"]) / "web/rose-suite.conf.txt"
     )
     shutil.copy(rose_suite_conf, web_conf_file)
 
