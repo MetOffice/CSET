af5c8eab7e205699fc622c9fcb8f775cb1731fa1
John M. Edwards <john.m.edwards@metoffice.gov.uk>
2025-06-06 17:32:16 +0100

    Corrections and improvments in the operators.

--- src/CSET/operators/plot.py
+++ src/CSET/operators/plot.py
@@ -1231,7 +1231,7 @@ def spatial_contour_plot(
     TypeError
         If the cube isn't a single cube.
     """
-    _spatial_plot("contourf", cube, filename, sequence_coordinate, stamp_coordinate)
+    _spatial_plot("contourf", cube, filename, sequence_coordinate, stamp_coordinate, **kwargs)
     return cube
 
 
@@ -1280,7 +1280,7 @@ def spatial_pcolormesh_plot(
     TypeError
         If the cube isn't a single cube.
     """
-    _spatial_plot("pcolormesh", cube, filename, sequence_coordinate, stamp_coordinate)
+    _spatial_plot("pcolormesh", cube, filename, sequence_coordinate, stamp_coordinate, **kwargs)
     return cube
 
 
