c5dfd5d2a3e78ac56120f399283d5ea7e60db8e1
James Frost <james.frost@metoffice.gov.uk>
2025-07-31 16:03:18 +0100

    WIP

---
+++ src/CSET/recipes/spatial_field.py
@@ -0,0 +1,172 @@
+# © Crown copyright, Met Office (2022-2025) and CSET contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Construct all the recipes."""
+
+import itertools
+
+from CSET.recipes import RecipeList, get_models
+
+
+# Generate recipes.
+def recipes(v: dict) -> RecipeList:
+    """Generate recipes from the given suite configuration."""
+    recipes = RecipeList()
+
+    # Load a list of model detail dictionaries.
+    models = get_models(v)
+
+    # Surface (2D) fields.
+    if v["SPATIAL_SURFACE_FIELD"]:
+        for model, field, method in itertools.product(
+            models, v["SURFACE_FIELDS"], v["SPATIAL_SURFACE_FIELD_METHOD"]
+        ):
+            recipes.add(
+                recipe="generic_surface_spatial_plot_sequence.yaml",
+                model_ids=model["id"],
+                variables={
+                    "VARNAME": field,
+                    "MODEL_NAME": model["name"],
+                    "METHOD": method,
+                    "SUBAREA_TYPE": v["SUBAREA_TYPE"] if v["SELECT_SUBAREA"] else None,
+                    "SUBAREA_EXTENT": v["SUBAREA_EXTENT"]
+                    if v["SELECT_SUBAREA"]
+                    else None,
+                },
+                aggregation=False,
+            )
+
+    # Pressure level fields.
+    if v["SPATIAL_PLEVEL_FIELD"]:
+        for model, field, plevel, method in itertools.product(
+            models,
+            v["PRESSURE_LEVEL_FIELDS"],
+            v["PRESSURE_LEVELS"],
+            v["SPATIAL_PLEVEL_FIELD_METHOD"],
+        ):
+            recipes.add(
+                recipe="generic_level_spatial_plot_sequence.yaml",
+                variables={
+                    "VARNAME": field,
+                    "LEVELTYPE": "pressure",
+                    "LEVEL": plevel,
+                    "MODEL_NAME": model["name"],
+                    "METHOD": method,
+                    "SUBAREA_TYPE": v["SUBAREA_TYPE"] if v["SELECT_SUBAREA"] else None,
+                    "SUBAREA_EXTENT": v["SUBAREA_EXTENT"]
+                    if v["SELECT_SUBAREA"]
+                    else None,
+                },
+                model_ids=model["id"],
+                aggregation=False,
+            )
+
+    # Model level fields
+    if v["SPATIAL_MLEVEL_FIELD"]:
+        for model, field, mlevel, method in itertools.product(
+            models,
+            v["MODEL_LEVEL_FIELDS"],
+            v["MODEL_LEVELS"],
+            v["SPATIAL_MLEVEL_FIELD_METHOD"],
+        ):
+            recipes.add(
+                recipe="generic_level_spatial_plot_sequence.yaml",
+                variables={
+                    "VARNAME": field,
+                    "LEVELTYPE": "model_level_number",
+                    "LEVEL": mlevel,
+                    "MODEL_NAME": model["name"],
+                    "METHOD": method,
+                    "SUBAREA_TYPE": v["SUBAREA_TYPE"] if v["SELECT_SUBAREA"] else None,
+                    "SUBAREA_EXTENT": v["SUBAREA_EXTENT"]
+                    if v["SELECT_SUBAREA"]
+                    else None,
+                },
+                model_ids=model["id"],
+                aggregation=False,
+            )
+
+    # Case aggregation.
+
+    # Create a list of case aggregation types.
+    AGGREGATION_TYPES = ["LEAD_TIME", "HOUR_OF_DAY", "VALIDITY_TIME", "ALL"]
+
+    # Surface (2D) fields.
+    for model, atype, field in itertools.product(
+        models, range(len(AGGREGATION_TYPES)), v["SURFACE_FIELDS"]
+    ):
+        if v["SPATIAL_SURFACE_FIELD_AGGREGATION"][atype]:
+            recipes.add(
+                recipe=f"generic_surface_spatial_plot_sequence_case_aggregation_mean_{AGGREGATION_TYPES[atype].lower()}.yaml",
+                variables={
+                    "VARNAME": field,
+                    "MODEL_NAME": model["name"],
+                    "SUBAREA_TYPE": v["SUBAREA_TYPE"] if v["SELECT_SUBAREA"] else None,
+                    "SUBAREA_EXTENT": v["SUBAREA_EXTENT"]
+                    if v["SELECT_SUBAREA"]
+                    else None,
+                },
+                model_ids=model["id"],
+                aggregation=True,
+            )
+
+    # Pressure level fields.
+    for model, atype, field, plevel in itertools.product(
+        models,
+        range(len(AGGREGATION_TYPES)),
+        v["PRESSURE_LEVEL_FIELDS"],
+        v["PRESSURE_LEVELS"],
+    ):
+        if v["SPATIAL_PLEVEL_FIELD_AGGREGATION"][atype]:
+            recipes.add(
+                recipe=f"generic_level_spatial_plot_sequence_case_aggregation_mean_{AGGREGATION_TYPES[atype].lower()}.yaml",
+                variables={
+                    "VARNAME": field,
+                    "LEVELTYPE": "pressure",
+                    "LEVEL": plevel,
+                    "MODEL_NAME": model["name"],
+                    "SUBAREA_TYPE": v["SUBAREA_TYPE"] if v["SELECT_SUBAREA"] else None,
+                    "SUBAREA_EXTENT": v["SUBAREA_EXTENT"]
+                    if v["SELECT_SUBAREA"]
+                    else None,
+                },
+                model_ids=model["id"],
+                aggregation=True,
+            )
+
+    # Model level fields.
+    for model, atype, field, mlevel in itertools.product(
+        models,
+        range(len(AGGREGATION_TYPES)),
+        v["MODEL_LEVEL_FIELDS"],
+        v["MODEL_LEVELS"],
+    ):
+        if v["SPATIAL_MLEVEL_FIELD_AGGREGATION"][atype]:
+            recipes.add(
+                recipe=f"generic_level_spatial_plot_sequence_case_aggregation_mean_{AGGREGATION_TYPES[atype].lower()}.yaml",
+                variables={
+                    "VARNAME": field,
+                    "LEVELTYPE": "model_level_number",
+                    "LEVEL": mlevel,
+                    "MODEL_NAME": model["name"],
+                    "SUBAREA_TYPE": v["SUBAREA_TYPE"] if v["SELECT_SUBAREA"] else None,
+                    "SUBAREA_EXTENT": v["SUBAREA_EXTENT"]
+                    if v["SELECT_SUBAREA"]
+                    else None,
+                },
+                model_ids=model["id"],
+                aggregation=True,
+            )
+
+    return recipes
