7a000cfab0a5c225841f0f95a4d8db6f4cb81deb
James Frost <james.frost@metoffice.gov.uk>
2025-08-11 20:06:52 +0100

    Add bake_recipes rose app
    This uses rose-bunch to bake multiple recipes in parallel inside of a
    single cylc job. This makes it significantly faster than running
    thousands of tiny jobs.
    
    To allow it to dynamically find the available jobs, cylc executes the
    baker.sh script, which writes a rose optional configuration specifying
    the recipes and parallelism, then runs the rose app using that
    configuration.

---
+++ src/CSET/cset_workflow/app/bake_recipes/bin/baker.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# Setup rose app for baking, then bake.
+set -euo pipefail
+
+# Use the appropriate recipes.
+optconfkey="$CYLC_TASK_CYCLE_POINT"
+if [ -n "${DO_CASE_AGGREGATION-}" ]; then
+    RECIPE_DIR=$CYLC_WORKFLOW_SHARE_DIR/cycle/$CYLC_TASK_CYCLE_POINT/aggregation_recipes
+    optconfkey="${optconfkey}-aggregation"
+else
+    RECIPE_DIR=$CYLC_WORKFLOW_SHARE_DIR/cycle/$CYLC_TASK_CYCLE_POINT/recipes
+fi
+export RECIPE_DIR
+
+# Determine parallelism.
+parallelism="$(nproc)"
+if [ "$CYLC_TASK_TRY_NUMBER" -gt 1 ]; then
+    # This is a retry; enable DEBUG logging and run in serial.
+    export LOGLEVEL="DEBUG"
+    parallelism=1
+fi
+
+# Get filenames without leading directory.
+recipes="$(cd "$RECIPE_DIR" && echo *.yaml)"
+
+# Write rose-bunch optional configuration.
+mkdir -p "$CYLC_WORKFLOW_RUN_DIR/app/bake_recipes/opt/"
+opt_conf="$CYLC_WORKFLOW_RUN_DIR/app/bake_recipes/opt/rose-app-${optconfkey}.conf"
+printf "[bunch]\npool-size=%s\n[bunch-args]\nrecipe_file=%s\n" "$parallelism" "$recipes" > "$opt_conf"
+unset opt_conf parallelism recipes
+
+# Run bake_recipes rose app.
+exec rose task-run -v --app-key=bake_recipes --opt-conf-key="${optconfkey}"
