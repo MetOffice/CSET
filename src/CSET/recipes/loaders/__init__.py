e3c4929db98a7f5f84f2af983a260e31aa94b571
James Frost <james.frost@metoffice.gov.uk>
2025-08-13 16:43:53 +0100

    Add CSET.recipes.loaders __init__.py
    It imports and re-exports the loaders for easy importing into the code.
    
    I originally tried using package introspection via pkgutil to find the
    packages at runtime, but it was rather complicated and hard to test, so
    I've gone with directly listing each file for now.

---
+++ src/CSET/recipes/loaders/__init__.py
@@ -0,0 +1,30 @@
+# © Crown copyright, Met Office (2022-2025) and CSET contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Recipe loaders.
+
+Each loader should provide a `load` function and be imported and added to
+__all__ below.
+"""
+
+from CSET.recipes.loaders import (
+    spatial_field,
+    test,
+)
+
+__all__ = [
+    "spatial_field",
+    "test",
+]
