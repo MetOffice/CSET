59b0bff6d2f6e20063fcddb6e7afb938b5954c7c
Simon Osborne <simon.osborne@metoffice.gov.uk>
2025-10-01 15:22:15 +0100

    SRO adding 1571 air frost presence diagnostic

--- src/CSET/loaders/spatial_field.py
+++ src/CSET/loaders/spatial_field.py
@@ -113,6 +113,21 @@ def load(conf: Config):
                 aggregation=False,
             )
 
+        # Air frost presence.
+        if conf.AIR_FROST_PRESENCE_SPATIAL_PLOT:
+            yield RawRecipe(
+                recipe="air_frost_presence_spatial_plot.yaml",
+                model_ids=model["id"],
+                variables={
+                    "MODEL_NAME": model["name"],
+                    "SUBAREA_TYPE": conf.SUBAREA_TYPE if conf.SELECT_SUBAREA else None,
+                    "SUBAREA_EXTENT": conf.SUBAREA_EXTENT
+                    if conf.SELECT_SUBAREA
+                    else None,
+                },
+                aggregation=False,
+            )
+
         # Surface winds on Beaufort Scale.
         if conf.SFC_WIND_BEAUFORT_SCALE_SPATIAL:
             yield RawRecipe(
