f7e188a49f6d69852bd473bfba488e8ebadd207d
Simon Osborne <simon.osborne@metoffice.gov.uk>
2025-10-03 10:02:13 +0100

    trailing whitespace removed

--- src/CSET/loaders/timeseries.py
+++ src/CSET/loaders/timeseries.py
@@ -121,7 +121,7 @@ def load(conf: Config):
             model_ids=[model["id"] for model in models],
             aggregation=False,
         )
- 
+
     # Thick fog presence
     if conf.THICK_FOG_PRESENCE_DOMAIN_MEAN_TIMESERIES:
         yield RawRecipe(
