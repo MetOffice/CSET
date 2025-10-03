6d64c1eec21969cfba7348bac68aaf6287aa53a0
David Flack <77390156+daflack@users.noreply.github.com>
2025-10-03 09:20:10 +0100

    Revert tutorial additions

--- src/CSET/loaders/spatial_field.py
+++ src/CSET/loaders/spatial_field.py
@@ -98,10 +98,10 @@ def load(conf: Config):
     # require individual loaders.
 
     for model in models:
-        # Dummy diagnostic.
-        if conf.NEW_AVIATION_DIAGNOSTIC_TUTORIAL_REBASE:
+        # Aviation Fog presence.
+        if conf.AVIATION_FOG_PRESENCE_SPATIAL_PLOT:
             yield RawRecipe(
-                recipe="new_aviation_diagnostic_tutorial_spatial_plot.yaml",
+                recipe="aviation_fog_presence_spatial_plot.yaml",
                 model_ids=model["id"],
                 variables={
                     "MODEL_NAME": model["name"],
