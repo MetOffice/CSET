01aafe8167e356d839af7bb66267358481039839
James Frost <james.frost@metoffice.gov.uk>
2025-10-14 12:32:59 +0100

    Add facet dropdowns to webpage

--- src/CSET/cset_workflow/app/install_website_skeleton/file/html/static/script.js
+++ src/CSET/cset_workflow/app/install_website_skeleton/file/html/static/script.js
@@ -66,19 +66,26 @@ function ensure_dual_frame() {
   dual_frame.classList.remove("hidden");
 }
 
-function add_to_sidebar(record) {
+function add_to_sidebar(record, facet_values) {
   const diagnostics_list = document.getElementById("diagnostics");
 
   // Add entry's display name.
-  const entry_title = document.createElement("h2")
+  const entry_title = document.createElement("h2");
   entry_title.textContent = record["title"];
 
   const facets = document.createElement("ul");
   for (const facet in record) {
     if (facet != "title" && facet != "path") {
+      // Create card for diagnostic.
       const li = document.createElement("li");
       li.textContent = `${facet}: ${record[facet]}`;
       facets.append(li);
+      // Record facet values.
+      if (!(facet in facet_values)) {
+        facet_values[facet] = new Set();
+      }
+      const values = facet_values[facet];
+      values.add(record[facet]);
     }
   }
 
@@ -103,7 +110,7 @@ function add_to_sidebar(record) {
       event.preventDefault();
       // Open new window for popup.
       if (position == "popup") {
-        window.open(`plots/${path}`, "_blank", "popup,width=800,height=600")
+        window.open(`plots/${path}`, "_blank", "popup,width=800,height=600");
         return;
       }
       // Set the appropriate frame layout.
@@ -125,8 +132,60 @@ function add_to_sidebar(record) {
   diagnostics_list.append(entry);
 }
 
+function add_facet_dropdowns(facet_values) {
+  const fieldset = document.getElementById("filter-facets");
 
-// Plot selection sidebar
+  for (const facet in facet_values) {
+    const label = document.createElement("label");
+    label.setAttribute("for", `facet-${facet}`);
+    label.textContent = facet;
+    const select = document.createElement("select");
+    select.id = `facet-${facet}`;
+    select.name = facet;
+    const null_option = document.createElement("option");
+    null_option.value = "";
+    null_option.defaultSelected = true;
+    null_option.textContent = "--- Any ---";
+    select.append(null_option);
+    // Sort facet values.
+    const values = Array.from(facet_values[facet]);
+    values.sort();
+    for (const value of values) {
+      const option = document.createElement("option");
+      option.textContent = value;
+      select.append(option);
+    }
+    select.addEventListener("change", updateFacetQuery);
+
+    // Add to DOM.
+    fieldset.append(label, select);
+  }
+}
+
+// Update query based on facet dropdown value.
+function updateFacetQuery(e) {
+  const facet = e.target.name;
+  const value = e.target.value;
+  const queryElem = document.getElementById("filter-query");
+  const query = queryElem.value;
+  let new_query;
+  // Construct regular expression matching facet condition.
+  const pattern = RegExp(`${facet}:\\s*('[^']*'|"[^"]*"|[^ \\t\\(\\)]+)`, "i");
+  if (value == "" && query.match(pattern)) {
+    // Facet unselected, remove from query.
+    new_query = query.replace(pattern, "");
+  } else if (query.match(pattern)) {
+    // Facet value selected, update the query.
+    new_query = query.replace(pattern, `${facet}:"${value}"`);
+  } else {
+    // Facet value selected, add the query.
+    new_query = query + ` ${facet}:"${value}"`;
+  }
+  queryElem.value = new_query.trim();
+  doSearch();
+}
+
+// Plot selection sidebar.
 function setup_plots_sidebar() {
   // Skip if there is no sidebar on page.
   if (!document.getElementById("plot-selector")) {
@@ -143,15 +202,17 @@ function setup_plots_sidebar() {
         return;
       }
       response.text().then((data) => {
+        const facet_values = {};
         // Remove throbber now download has finished.
-        document.querySelector("#diagnostics > loading-throbber").remove()
+        document.querySelector("#diagnostics > loading-throbber").remove();
         for (let line of data.split("\n")) {
           line = line.trim();
           // Skip blank lines.
           if (line.length) {
-            add_to_sidebar(JSON.parse(line));
+            add_to_sidebar(JSON.parse(line), facet_values);
           }
         }
+        add_facet_dropdowns(facet_values);
         // Do search if we already have a query specified in the URL.
         const search = document.getElementById("filter-query");
         const params = new URLSearchParams(document.location.search);
@@ -160,7 +221,7 @@ function setup_plots_sidebar() {
           search.value = initial_query;
           doSearch();
         }
-      })
+      });
     })
     .catch((err) => {
       // Catch non-HTTP fetch errors.
@@ -181,18 +242,26 @@ function setup_clear_view_button() {
   clear_view_button.addEventListener("click", clear_frames);
 }
 
+function setup_clear_search_button() {
+  const clear_search_button = document.getElementById("clear-query");
+  clear_search_button.addEventListener("click", () => {
+    document.getElementById("filter-query").value = "";
+    doSearch();
+  });
+}
+
 // Parse the query, returning a comparison function.
 function parse_query(query) {
   // TODO: Parse the query into an easily compared form.
-  const title = query.toLowerCase()
+  const title = query.toLowerCase();
 
   // Returns true or false for a given event based on the query.
   function test(entry) {
     // TODO: Implement comparison of facets.
-    return entry.textContent.includes(title)
+    return entry.textContent.includes(title);
   }
 
-  return test
+  return test;
 }
 
 // Filter the displayed diagnostics by the query.
@@ -202,18 +271,18 @@ function doSearch() {
   const url = new URL(document.location.href);
   query ? url.searchParams.set("q", query) : url.searchParams.delete("q");
   // Updates the URL without reloading the page.
-  history.pushState(history.state, "", url.href)
+  history.pushState(history.state, "", url.href);
 
   console.log("Search query:", query);
-  const test = parse_query(query)
+  const test = parse_query(query);
 
   // Filter all entries.
   for (const entry of document.querySelectorAll("#diagnostics > li")) {
     // Show entries matching filter and hide entries that don't.
     if (test(entry)) {
-      entry.classList.remove("hidden")
+      entry.classList.remove("hidden");
     } else {
-      entry.classList.add("hidden")
+      entry.classList.add("hidden");
     }
   }
 }
@@ -243,5 +312,6 @@ function setup_search() {
 // Run everything.
 setup_description_toggle_button();
 setup_clear_view_button();
+setup_clear_search_button();
 setup_plots_sidebar();
 setup_search();
