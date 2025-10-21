2557df626a8ca684598ab8beb382903c3f7a69e9
daflack <david.flack1@metoffice.gov.uk>
2025-10-21 16:18:37 +0100

    Add loaders for fog recipes

--- src/CSET/loaders/spatial_field.py
+++ src/CSET/loaders/spatial_field.py
@@ -113,6 +113,21 @@ def load(conf: Config):
                 aggregation=False,
             )
 
+        # Fog presence.
+        if conf.FOG_PRESENCE_SPATIAL_PLOT:
+            yield RawRecipe(
+                recipe="fog_presence_spatial_plot.yaml",
+                model_ids=model["id"],
+                variables={
+                    "MODEL_NAME": model["name"],
+                    "SUBAREA_TYPE": conf.SUBAREA_TYPE if conf.SELECT_SUBAREA else None,
+                    "SUBAREA_EXTENT": conf.SUBAREA_EXTENT
+                    if conf.SELECT_SUBAREA
+                    else None,
+                },
+                aggregation=False,
+            )
+
         # Rain presence.
         if conf.RAIN_PRESENCE_SPATIAL_PLOT:
             yield RawRecipe(
