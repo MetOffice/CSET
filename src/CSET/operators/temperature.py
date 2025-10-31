98b5fe285acfc98eac7bdb83050647cb28581f47
daflack <david.flack1@metoffice.gov.uk>
2025-10-31 11:44:22 +0000

    Fix spatial plot recipe and diagnostic for RH not in range 0-1

--- src/CSET/operators/temperature.py
+++ src/CSET/operators/temperature.py
@@ -32,32 +32,29 @@ def equivalent_potential_temperature_pressure_levels(relative_humidity, temperat
     Rv = 461.0
     cpd = 1005.7
     Lv = 2.501e6
+    e0 = 6.1078  # hPa
     for RH, T in zip(
         iter_maybe(relative_humidity), iter_maybe(temperature), strict=True
     ):
-        theta_e = T.copy()
-        theta_e.data[:] = 0.0
-
         logging.debug("Convert relative humidity to mixing ratio.")
-        es = T.copy()
-        TT = T.copy()
-        mr = T.copy()
-        e0 = 6.1078  # hPa
+        if RH.data.max() > 100:
+            RH /= 100.0
         TT = 17.27 * (T - 273.16) / (T - 35.86)
-        es.data = e0 * np.exp(TT.data)
-        es.rename("saturation_vapour_pressure")
-        es.units = "hPa"
-        mr = RH * (0.622 * (es / (T.coord("pressure").points[:] - es)))
+        es = e0 * np.exp(TT.core_data())
+        mr = RH * (0.622 * (es / (T.coord("pressure").points - es)))
         mr.rename("mixing_ratio")
         mr.units = "1"
         logging.debug("Calculate equivalent potential temperature on pressure levels.")
-        first_term = (p0 / T.coord("pressure").points[:]) ** (Rd / cpd)
+        first_term = (p0 / T.coord("pressure").points) ** (Rd / cpd)
         second_term_power = -(mr * Rv) / cpd
-        second_term = RH.data**second_term_power.data
+        second_term = RH.core_data() ** second_term_power.core_data()
         third_term_power = Lv * mr / (cpd * T)
-        third_term = np.exp(third_term_power.data)
+        third_term = np.exp(third_term_power.core_data())
         theta_e = T * first_term * second_term * third_term
+        print(theta_e.data.max())
+        print(theta_e.data.min())
         theta_e.rename("equivalent_potential_temperature")
+        theta_e.units = "K"
         theta_e_list.append(theta_e)
 
     if len(theta_e_list) == 1:
