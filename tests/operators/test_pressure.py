54adb2cd8cc2fe220b571e9fed03a2a217c4fc4f
daflack <david.flack1@metoffice.gov.uk>
2026-01-07 12:25:59 +0000

    Adds tests for relative_humidity_to_vapour_pressure

--- tests/operators/test_pressure.py
+++ tests/operators/test_pressure.py
@@ -72,3 +72,67 @@ def test_vapour_pressure_cube_list(temperature_for_conversions_cube):
     actual_cubelist = pressure.vapour_pressure(input_cubelist)
     for cube_a, cube_b in zip(expected_list, actual_cubelist, strict=True):
         assert np.allclose(cube_a.data, cube_b.data, rtol=1e-6, atol=1e-2)
+
+
+def test_relative_humidity_to_vapour_pressure(
+    temperature_for_conversions_cube, relative_humidity_for_conversions_cube
+):
+    """Test calculation of vapour pressure from relative humidity."""
+    expected_data = pressure.vapour_pressure(temperature_for_conversions_cube) * (
+        relative_humidity_for_conversions_cube / 100.0
+    )
+    assert np.allclose(
+        expected_data.data,
+        pressure.relative_humidity_to_vapour_pressure(
+            temperature_for_conversions_cube, relative_humidity_for_conversions_cube
+        ).data,
+        rtol=1e-6,
+        atol=1e-2,
+    )
+
+
+def test_relative_humidity_to_vapour_pressure_name(
+    temperature_for_conversions_cube, relative_humidity_for_conversions_cube
+):
+    """Test naming of vapour pressure cube."""
+    expected_name = "vapour_pressure"
+    assert (
+        expected_name
+        == pressure.relative_humidity_to_vapour_pressure(
+            temperature_for_conversions_cube, relative_humidity_for_conversions_cube
+        ).name()
+    )
+
+
+def test_relative_humidity_to_vapour_pressure_units(
+    temperature_for_conversions_cube, relative_humidity_for_conversions_cube
+):
+    """Test units of vapour pressure cube."""
+    expected_units = cf_units.Unit("hPa")
+    assert (
+        expected_units
+        == pressure.relative_humidity_to_vapour_pressure(
+            temperature_for_conversions_cube, relative_humidity_for_conversions_cube
+        ).units
+    )
+
+
+def test_relative_humidity_to_vapour_pressure_cubelist(
+    temperature_for_conversions_cube, relative_humidity_for_conversions_cube
+):
+    """Test calculation of vapour pressure from relative humidity as a CubeList."""
+    expected_data = pressure.vapour_pressure(temperature_for_conversions_cube) * (
+        relative_humidity_for_conversions_cube / 100.0
+    )
+    expected_list = iris.cube.CubeList([expected_data, expected_data])
+    temperature_input_list = iris.cube.CubeList(
+        [temperature_for_conversions_cube, temperature_for_conversions_cube]
+    )
+    relative_humidity_input_list = iris.cube.CubeList(
+        [relative_humidity_for_conversions_cube, relative_humidity_for_conversions_cube]
+    )
+    actual_cubelist = pressure.relative_humidity_to_vapour_pressure(
+        temperature_input_list, relative_humidity_input_list
+    )
+    for cube_a, cube_b in zip(expected_list, actual_cubelist, strict=True):
+        assert np.allclose(cube_a.data, cube_b.data, rtol=1e-6, atol=1e-2)
