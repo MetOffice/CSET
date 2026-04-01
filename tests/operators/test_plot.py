cf23a2231ec7e9852f52c0ad63d31eb945d07ec8
Carol Halliwell <carol.halliwell@metoffice.gov.uk>
2026-04-01 09:57:34 +0100

    more unit tests

--- tests/operators/test_plot.py
+++ tests/operators/test_plot.py
@@ -756,6 +756,7 @@ def test_plot_power_spectrum_series_coord_physical_wavenumber(
     power_spectrum_cube, tmp_working_dir
 ):
     """Testing series_coordinate."""
+    # NOT SURE THIS IS CORRECT
     # assert power_spectrum_cube.coords("frequency")
 
     # Get original frequency coordinate
@@ -778,7 +779,34 @@ def test_plot_power_spectrum_series_coord_physical_wavenumber(
     power_spectrum_cube.add_aux_coord(physical_wavenumber_coord, dim)
 
     plot.plot_line_series(
-        power_spectrum_cube, series_coordinate="physical_wavenumber", filename="test"
+        power_spectrum_cube, series_coordinate="frequency", filename="test"
+    )
+    assert Path("test.png").is_file()
+
+
+def test_plot_power_spectrum_series_coord_wavelength(
+    power_spectrum_cube, tmp_working_dir
+):
+    """Testing series_coordinate."""
+    # Get original frequency coordinate
+
+    freq = power_spectrum_cube.coord("frequency")
+    dim = power_spectrum_cube.coord_dims(freq)
+
+    # Construct a synthetic wavelength (simple proportional mapping for testing)
+    k = np.linspace(0.1, 2.0, len(freq.points))
+
+    wavelength_coord = iris.coords.DimCoord(
+        points=k,
+        long_name="wavelength",
+        units="m-1",
+    )
+
+    # Add the new coordinate on the same dimension as frequency
+    power_spectrum_cube.add_aux_coord(wavelength_coord, dim)
+
+    plot.plot_line_series(
+        power_spectrum_cube, series_coordinate="frequency", filename="test"
     )
     assert Path("test.png").is_file()
 
