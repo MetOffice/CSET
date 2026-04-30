8a7d2b7f1d683a1847dba4b2f67b18e2dfc02b07
daflack <david.flack1@metoffice.gov.uk>
2026-04-30 16:02:22 +0100

    Near full version of a slow CURV operator

--- src/CSET/operators/curvature.py
+++ src/CSET/operators/curvature.py
@@ -18,12 +18,15 @@ The diagnostics here are separate implementations of CURV for both
 global models and limited-area models.
 """
 
+import logging
+
+import iris
 import numpy as np
 
 
-def lat_lon_identifier(original_lat, original_lon, distance, bearing):
+def _lat_lon_identifier(original_lat, original_lon, distance, bearing):
     """Find the latitude and longitude of new points based on great circle distance."""
-    R = 6371.0
+    R = 6371.0  # km.
     lat_rad = np.radians(original_lat)
     lon_rad = np.radians(original_lon)
     bearing_rad = np.radians(bearing)
@@ -37,63 +40,57 @@ def lat_lon_identifier(original_lat, original_lon, distance, bearing):
         np.cos(distance / R) - np.sin(lat_rad) * np.sin(new_lat),
     )
 
-    new_lon = np.where(new_lon > 180.0, new_lon - 360.0, new_lon)
+    new_lat = np.degrees(new_lat)
+    new_lon = np.degrees(new_lon)
+    new_lon = np.where(new_lon > 360.0, new_lon - 360.0, new_lon)
 
     return new_lat, new_lon
 
 
-# def curvature_function(central, surrounding, tolerance, radial_points):
-#    """Calculate the intermediate data for CURV diagnostic."""
-#    shape_of_surrounding = surrounding.shape
-#    binary_difference = np.zeros(shape_of_surrounding)
-#    for rp in range(radial_points):
-#        binary_difference[:, rp] = np.where(
-#            surrounding[:, rp] < central[:] + tolerance, 1, 0
-#        )
-#        binary_difference[:, rp] = np.where(
-#            surroudnig[:, rp] > central[:] - tolerance, -1, binary_difference[:, rp]
-#        )
-#    curvature = bindary_difference.sum(axis=1)
-#    return curvature
+def curv(central, radius, num_radial_points=16, tol=0):
+    """Calculate the CURV diagnostic."""
+    if (num_radial_points == 8) or (num_radial_points == 16):
+        bearing = np.linspace(0.0, 360.0, num_radial_points + 1)[:-1]
+    else:
+        raise ValueError(
+            f"Number of radial points should be 8 or 16. You have provided {num_radial_points}."
+        )
 
+    logging.info(
+        f"CURV calculated base on {num_radial_points} radial points, a {radius} km radius and {tol} tolerance."
+    )
 
-# def curv(
-#    radius, num_radial_points, north, south, east, west, tolerance, cube, outputfile
-# ):
-#    """Calculate the CURV diagnostic."""
-#    if (num_radial_points != 8) or (num_radial_points != 16):
-#        raise ValueError(
-#            f"Number of radial points should be 8 or 16. You have provided {num_radial_points}."
-#        )
-#    else:
-#        bearing = np.linspace(0.0, 360.0, num_radial_points + 1)
-#
-#    data_area = [north, west, south, east]
-#
-#    logging.info(
-#        f"CURV calculated base on {num_radial_points} radial points, a {radius} km radius, with {tolerance} tolerance."
-#    )
-#    logging.info(f"CURV calculated over area {data_area}.")
-#
-#    extracted_data = cube.extract(lon)
-#    extract_lats = extract_data.extract(lat)
-#
-#    points = np.size(extract_data.core_data())
-#
-#    curv = np.zeros(points)
-#
-#    new_lats = np.zeros((points, num_radial_points))
-#    new_lons = np.zeros((points, num_radial_points))
-#
-#    new_vals = np.zeros((points, num_radial_points))
-#
-#    for i in range(num_radial_points):
-#        new_lats[:, i], new_lons[:, i] = lat_lon_identifier(
-#            lats, lons, radius, bearing[i]
-#        )
-#
-#        new_vals[:, i] = cube.interpolate(new_lats[:, i], new_lons[:, i])
-#
-#    curv = curvature_function(cube, new_vals, tolerance, num_radial_points)
-#
-#    return curv
+    surroundings = iris.cube.CubeList([])
+    for b in bearing:
+        surround = central.copy()
+        # Need to try and avoid this loop for whole domain if possible, also need to raise time and realization bits as well.
+        for lat_number, lat in enumerate(central.slices_over("latitude")):
+            for lon_number, lon in enumerate(lat.slices_over("longitude")):
+                new_lat, new_lon = _lat_lon_identifier(
+                    lon.coord("latitude").points,
+                    lon.coord("longitude").points,
+                    radius,
+                    b,
+                )
+                # Need to get interpolation better.
+                surround.data[lat_number, lon_number] = central.data[
+                    np.abs(central.coord("latitude").points - new_lat[0]).argmin(),
+                    np.abs(central.coord("longitude").points - new_lon[0]).argmin(),
+                ]
+        surround.add_aux_coord(
+            iris.coords.DimCoord(b, long_name="bearing", units="degrees")
+        )
+        surroundings.append(surround)
+    surroundings.merge()
+    print(surroundings)
+
+    # curv = surroundings.copy()
+    # curv -= central
+
+    # curv.data[curv.data > tol]=-1.0
+    # curv.data[curv.data < tol]=1.0
+
+    # curv.collapsed('bearing',iris.analysis.SUM)
+    # curv.rename("CURV")
+    # curv.units="1"
+    # return curv
