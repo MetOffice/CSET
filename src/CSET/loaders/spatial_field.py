67f34bc802a27d5477c675a0b8b1db15b4f652b1
daflack <david.flack1@metoffice.gov.uk>
2026-05-01 10:19:12 +0100

    Update loaders

--- src/CSET/loaders/spatial_field.py
+++ src/CSET/loaders/spatial_field.py
@@ -754,6 +754,7 @@ def load(conf: Config):
                 variables={
                     "VARNAME": field,
                     "MODEL_NAME": model["name"],
+                    "METHOD": method,
                     "SUBAREA_TYPE": conf.SUBAREA_TYPE if conf.SELECT_SUBAREA else None,
                     "SUBAREA_EXTENT": conf.SUBAREA_EXTENT
                     if conf.SELECT_SUBAREA
@@ -770,6 +771,7 @@ def load(conf: Config):
                 variables={
                     "VARNAME": field,
                     "MODEL_NAME": model["name"],
+                    "METHOD": method,
                     "SUBAREA_TYPE": conf.SUBAREA_TYPE if conf.SELECT_SUBAREA else None,
                     "SUBAREA_EXTENT": conf.SUBAREA_EXTENT
                     if conf.SELECT_SUBAREA
