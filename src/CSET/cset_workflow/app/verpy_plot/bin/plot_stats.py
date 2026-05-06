57c33b0ef2176b37ac9689aa70006fafd367c173
Jorge Bornemann <Jorge.Bornemann@niwa.co.nz>
2026-05-06 02:08:33 +0000

    Add guard to prevent execution of top-level code in multiprocessing, set arguments as required

--- src/CSET/cset_workflow/app/verpy_plot/bin/plot_stats.py
+++ src/CSET/cset_workflow/app/verpy_plot/bin/plot_stats.py
@@ -3,8 +3,7 @@
 """
 Produce Verpy Plots.
 
-Creates verpy options dictionaries from configuration files and environment vbles,
-then runs verpy to create the plots
+Small utility to produce plots from METPlus results.
 """
 
 import argparse
@@ -14,35 +13,50 @@ import os
 import VerPy
 import VerPy.conf2opts
 
-parser = argparse.ArgumentParser(
-    prog="verpy_plot",
-    description="produce verpy plots from the metplus results database",
-    epilog="Only intended to run as part of the verpy_plot app in cset_workflow",
-)
 
-parser.add_argument("--conf", action="append")
-parser.add_argument("--outdir")
-parser.add_argument("--start")
-parser.add_argument("--end")
-parser.add_argument("--expid")
+def main():
+    """
+    Produce Verpy Plots.
 
-
-args = parser.parse_args()
-
-opts_dicts, scard_dict = VerPy.conf2opts.conf2opts(args.conf)
-
-for options in opts_dicts:
-    options["start"] = args.start
-    options["end"] = args.end
-    options["expid"] = args.expid
-    options["source"] = (
-        f"{os.getenv('TABLENAME')}@{os.getenv('DB_DIR')}/{os.getenv('DB_NAME')}"
+    Create verpy options dictionaries from configuration files and environment vbles.
+    Run verpy to create the plots.
+    """
+    parser = argparse.ArgumentParser(
+        prog="verpy_plot",
+        description="produce verpy plots from the metplus results database",
+        epilog="Only intended to run as part of the verpy_plot app in cset_workflow",
     )
-    print(f"Options Dictionary: {options}")
-    VerPy.job.run(args.outdir, options)
-    # Create a json metadata file in outdir
-    json_filename = f"{args.outdir}/{options['jobid']}/meta.json"
-    print(f"writing metadata to json file: {json_filename}")
-    metadata_dict = {"title": "Metplus Point Stat plots", "category": "Metplus plots"}
-    with open(json_filename, "w") as jf:
-        json.dump(metadata_dict, jf, indent=2)
+
+    parser.add_argument("--conf", action="append", required=True)
+    parser.add_argument("--outdir", required=True)
+    parser.add_argument("--start", required=True)
+    parser.add_argument("--end", required=True)
+    parser.add_argument("--expid", required=True)
+
+    args = parser.parse_args()
+
+    opts_dicts, scard_dict = VerPy.conf2opts.conf2opts(args.conf)
+
+    for options in opts_dicts:
+        options["start"] = args.start
+        options["end"] = args.end
+        options["expid"] = args.expid
+        options["source"] = (
+            f"{os.getenv('TABLENAME')}@{os.getenv('DB_DIR')}/{os.getenv('DB_NAME')}"
+        )
+        print(f"Options Dictionary: {options}")
+        VerPy.job.run(args.outdir, options)
+
+        # Create a json metadata file in outdir
+        json_filename = f"{args.outdir}/{options['jobid']}/meta.json"
+        print(f"writing metadata to json file: {json_filename}")
+        metadata_dict = {
+            "title": "Metplus Point Stat plots",
+            "category": "Metplus plots",
+        }
+        with open(json_filename, "w") as jf:
+            json.dump(metadata_dict, jf, indent=2)
+
+
+if __name__ == "__main__":
+    main()
