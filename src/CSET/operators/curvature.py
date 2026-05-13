abeb3aceecd79304bbba5b0e222995d191af1b0e
daflack <david.flack1@metoffice.gov.uk>
2026-05-13 15:44:25 +0100

    Updates recipe to use the faster operator, and creates a tentative fast operator
    This commit contains the first working version of global_curv in
    global_curv_version2, global_curv is slow and has not yet produced
    output.

--- src/CSET/operators/curvature.py
+++ src/CSET/operators/curvature.py
@@ -23,6 +23,8 @@ import logging
 import iris
 import numpy as np
 
+from CSET._common import iter_maybe
+
 
 def _lat_lon_identifier(original_lat, original_lon, distance, bearing):
     """Find the latitude and longitude of new points based on great circle distance."""
@@ -59,38 +61,94 @@ def global_curv(central, radius, num_radial_points=16, tol=0):
     logging.info(
         f"CURV calculated base on {num_radial_points} radial points, a {radius} km radius and {tol} tolerance."
     )
+    curv_cubes = iris.cube.CubeList([])
+    for cube in iter_maybe(central):
+        surroundings = iris.cube.CubeList([])
+        for b in bearing:
+            surround = cube.copy()
+            # Need to try and avoid this loop for whole domain if possible, also need to raise time and realization bits as well.
+            for lat_number, lat in enumerate(cube.slices_over("latitude")):
+                for lon_number, lon in enumerate(lat.slices_over("longitude")):
+                    new_lat, new_lon = _lat_lon_identifier(
+                        lon.coord("latitude").points,
+                        lon.coord("longitude").points,
+                        radius,
+                        b,
+                    )
+                    # Need to get interpolation better.
+                    surround.data[:, lat_number, lon_number] = cube.data[
+                        :,
+                        np.abs(cube.coord("latitude").points - new_lat[0]).argmin(),
+                        np.abs(cube.coord("longitude").points - new_lon[0]).argmin(),
+                    ]
+            surround.add_aux_coord(
+                iris.coords.DimCoord(b, long_name="bearing", units="degrees")
+            )
+            surroundings.append(surround)
+        surroundings.merge()
+        print(surroundings)
 
-    surroundings = iris.cube.CubeList([])
-    for b in bearing:
-        surround = central.copy()
-        # Need to try and avoid this loop for whole domain if possible, also need to raise time and realization bits as well.
-        for lat_number, lat in enumerate(central.slices_over("latitude")):
-            for lon_number, lon in enumerate(lat.slices_over("longitude")):
-                new_lat, new_lon = _lat_lon_identifier(
-                    lon.coord("latitude").points,
-                    lon.coord("longitude").points,
-                    radius,
-                    b,
-                )
-                # Need to get interpolation better.
-                surround.data[lat_number, lon_number] = central.data[
-                    np.abs(central.coord("latitude").points - new_lat[0]).argmin(),
-                    np.abs(central.coord("longitude").points - new_lon[0]).argmin(),
-                ]
-        surround.add_aux_coord(
-            iris.coords.DimCoord(b, long_name="bearing", units="degrees")
+        curv = surroundings[0].copy()
+        curv -= cube
+
+        curv.data[curv.data > tol] = -1.0
+        curv.data[curv.data < tol] = 1.0
+
+        curv.collapsed("bearing", iris.analysis.SUM)
+        curv.rename(f"CURV_calculated_from_{num_radial_points}_radial_points")
+        curv.units = "1"
+        curv_cubes.append(curv)
+    if len(curv_cubes) == 1:
+        return curv_cubes[0]
+    else:
+        return curv_cubes
+
+
+def global_curv_versiontwo(central, radius, num_radial_points=16, tol=0.0):
+    """Calculate the CURV diagnostic for a global model VERSION 2 for faster calculation speed."""
+    if (num_radial_points == 8) or (num_radial_points == 16):
+        bearing = np.linspace(0.0, 360.0, num_radial_points + 1)[:-1]
+    else:
+        raise ValueError(
+            f"Number of radial points should be 8 or 16. You have provided {num_radial_points}."
         )
-        surroundings.append(surround)
-    surroundings.merge()
-    print(surroundings)
 
-    curv = surroundings[0].copy()
-    curv -= central
-
-    curv.data[curv.data > tol] = -1.0
-    curv.data[curv.data < tol] = 1.0
-
-    curv.collapsed("bearing", iris.analysis.SUM)
-    curv.rename(f"CURV_calculated_from_{num_radial_points}_radial_points")
-    curv.units = "1"
-    return curv
+    logging.info(
+        f"CURV calculated base on {num_radial_points} radial points, a {radius} km radius and {tol} tolerance."
+    )
+    curv_cubes = iris.cube.CubeList([])
+    for cube in iter_maybe(central):
+        curv = cube.copy()
+        surroundings = []
+        lats, lons = np.meshgrid(
+            cube.coord("latitude").points, cube.coord("longitude").points
+        )
+        for b in bearing:
+            target_data = cube.copy()
+            new_lat, new_lon = _lat_lon_identifier(
+                lats,
+                lons,
+                radius,
+                b,
+            )
+            new_lat_points = new_lat[0, :]
+            new_lon_points = new_lon[:, 0]
+            surround = target_data.interpolate(
+                [("latitude", new_lat_points), ("longitude", new_lon_points)],
+                iris.analysis.Nearest(),
+            )
+            surroundings.append(surround.data)
+        surroundings = np.array(surroundings)
+        curv_data = surroundings - cube.data
+        curv_data_binary = np.zeros(curv_data.shape)
+        curv_data_binary[curv_data > tol] = -1.0
+        curv_data_binary[curv_data < tol] = 1.0
+        curv_data_collapsed = np.sum(curv_data_binary, 0)
+        curv.data = curv_data_collapsed
+        curv.rename(f"CURV_calculated_from_{num_radial_points}_radial_points")
+        curv.units = "1"
+        curv_cubes.append(curv)
+    if len(curv_cubes) == 1:
+        return curv_cubes[0]
+    else:
+        return curv_cubes
