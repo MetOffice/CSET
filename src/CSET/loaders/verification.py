0f0c4c6e10c8d2e1aa2fb31883aca0a637e398df
daflack <david.flack1@metoffice.gov.uk>
2026-05-20 11:45:02 +0100

    Adds timeseries recipe and plumbing into workflow

--- src/CSET/loaders/verification.py
+++ src/CSET/loaders/verification.py
@@ -45,3 +45,21 @@ def load(conf: Config):
                 model_ids=[base_model["id"], model["id"]],
                 aggregation=False,
             )
+
+    if conf.TIMESERIES_SCORES_RMSE:
+        base_model = models[0]
+        for model, field in itertools.product(models[1:], conf.SURFACE_FIELDS):
+            yield RawRecipe(
+                recipe="timeseries_surface_rmse_scores.yaml",
+                variables={
+                    "VARNAME": field,
+                    "BASE_MODEL": base_model["name"],
+                    "OTHER_MODEL": model["name"],
+                    "SUBAREA_TYPE": conf.SUBAREA_TYPE if conf.SELECT_SUBAREA else None,
+                    "SUBAREA_EXTENT": conf.SUBAREA_EXTENT
+                    if conf.SELECT_SUBAREA
+                    else None,
+                },
+                model_ids=[base_model["id"], model["id"]],
+                aggregation=False,
+            )
