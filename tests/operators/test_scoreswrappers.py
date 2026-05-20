9f52210dd25316e093e15ebeefaacdb7776aec65
daflack <david.flack1@metoffice.gov.uk>
2026-05-20 15:19:03 +0100

    Test investigation notes

---
+++ tests/operators/test_scoreswrappers.py
@@ -0,0 +1,153 @@
+# © Crown copyright, Met Office (2022-2026) and CSET contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Test miscellaneous operators."""
+
+import datetime
+
+import iris
+import iris.analysis.calculus
+import iris.coords
+import iris.cube
+import iris.exceptions
+import numpy as np
+import pytest
+
+from CSET.operators import misc, scoreswrappers
+
+
+def test_scores_rmse(cube: iris.cube.Cube):
+    """Test taking the rmse between two cubes."""
+    # Data preparation.
+    other_cube = cube.copy()
+    del other_cube.attributes["cset_comparison_base"]
+    cubes = iris.cube.CubeList([cube, other_cube])
+
+    # Take difference.
+    rmse_cube = scoreswrappers.scores_rmse(cubes)
+
+    # As both cubes use the same data, check the rmse is zero.
+    assert isinstance(rmse_cube, iris.cube.Cube)
+    assert np.allclose(rmse_cube.data, np.zeros_like(rmse_cube.data), atol=1e-9)
+    assert rmse_cube.standard_name is None
+    assert rmse_cube.long_name == "RMSE_of_air_temperature"
+
+
+def test_scores_rmse_no_time_coord(cube):
+    """RMSE of cubes with no time coordinate."""
+    c1 = cube.extract(iris.Constraint(time=datetime.datetime(2022, 9, 21, 3, 30)))
+    c1.remove_coord("time")
+    c2 = c1.copy()
+    del c2.attributes["cset_comparison_base"]
+    cubes = iris.cube.CubeList([c1, c2])
+    rmse_cube = scoreswrappers.scores_rmse(cubes)
+    assert isinstance(rmse_cube, iris.cube.Cube)
+    assert np.allclose(rmse_cube.data, np.zeros_like(rmse_cube.data), atol=1e-9)
+
+
+def test_scores_rmse_no_common_points(cube):
+    """Test exception when there are no common time points between cubes."""
+    other_cube = cube.copy()
+    # Offset times by 6 hours.
+    new_times = other_cube.coord("time").points.copy()
+    new_times += 6
+    other_cube.coord("time").points = new_times
+    del other_cube.attributes["cset_comparison_base"]
+    cubes = iris.cube.CubeList([cube, other_cube])
+    with pytest.raises(ValueError, match="No common time points found!"):
+        scoreswrappers.scores_rmse(cubes)
+
+
+def test_scores_rmse_incorrect_number_of_cubes(cube):
+    """Test exception when incorrect number of cubes provided."""
+    no_cubes = iris.cube.CubeList([])
+    with pytest.raises(ValueError, match="cubes should contain exactly 2 cubes."):
+        scoreswrappers.scores_rmse(no_cubes)
+
+    one_cube = iris.cube.CubeList([cube])
+    with pytest.raises(ValueError, match="cubes should contain exactly 2 cubes."):
+        scoreswrappers.scores_rmse(one_cube)
+
+    three_cubes = iris.cube.CubeList([cube, cube, cube])
+    with pytest.raises(ValueError, match="cubes should contain exactly 2 cubes."):
+        scoreswrappers.scores_rmse(three_cubes)
+
+
+def test_scores_rmse_different_data_shape_regrid(cube):
+    """Test when data shape differs, but gets regridded.
+
+    For any cube shapes differ.
+    """
+    rearranged_cube = cube.copy()
+    rearranged_cube = rearranged_cube[:, :, 1:]
+    del cube.attributes["cset_comparison_base"]
+    cubes = iris.cube.CubeList([rearranged_cube, cube])
+    # Need to preserve coordinates to test shape.
+    rmse = scoreswrappers.scores_rmse(
+        cubes, preserved_coordinates=["time", "grid_latitude", "grid_longitude"]
+    )
+    assert isinstance(rmse, iris.cube.Cube)
+    assert rmse.shape == cube.shape
+    assert rmse.shape != rearranged_cube.shape
+
+
+def test_rmse_grid_staggering_regrid(cube):
+    """Test when data considered on staggered grid, so gets regridded."""
+    rearranged_cube = cube.copy()
+    rearranged_cube.rename("eastward_wind_at_10m")
+    del cube.attributes["cset_comparison_base"]
+    cubes = iris.cube.CubeList([rearranged_cube, cube])
+    # Need to preserve coordinates to test shape.
+    rmse = scoreswrappers.scores_rmse(
+        cubes, preserved_coordinates=["time", "grid_latitude", "grid_longitude"]
+    )
+    assert isinstance(rmse, iris.cube.Cube)
+    assert rmse.shape == cube.shape
+
+
+def test_difference_different_model_types(cube):
+    """Other cube is flipped when model types differ."""
+    flipped = cube.copy()
+    flipped_coord = flipped.coord("grid_latitude")
+    flipped_coord.points = np.flip(flipped_coord.points)
+    flipped.data = np.flip(flipped.data, flipped_coord.cube_dims(flipped))
+    del flipped.attributes["cset_comparison_base"]
+    cubes = iris.cube.CubeList([cube, flipped])
+
+    # Take difference.
+    difference_cube = misc.difference(cubes)
+
+    assert isinstance(difference_cube, iris.cube.Cube)
+    # As both cubes use the same data, check the difference is zero.
+    assert np.allclose(
+        difference_cube.data, np.zeros_like(difference_cube.data), atol=1e-9
+    )
+
+
+def test_difference_flip_pressure_order(transect_source_cube_readonly):
+    """Test that pressure coord is flipped if discreasing."""
+    flipped = transect_source_cube_readonly.copy()
+    flipped_coord = flipped.coord("pressure")
+    flipped_coord.points = np.flip(flipped_coord.points)
+    flipped.data = np.flip(flipped.data, flipped_coord.cube_dims(flipped))
+    del flipped.attributes["cset_comparison_base"]
+    cubes = iris.cube.CubeList([transect_source_cube_readonly, flipped])
+
+    # Take difference.
+    difference_cube = misc.difference(cubes)
+
+    # If flipped correctly, difference should be zero as same cubes.
+    assert np.allclose(
+        difference_cube.data, np.zeros_like(difference_cube.data), atol=1e-9
+    )
