e134137690fb18c6aada77584e4e2e78a72f5052
Carol Halliwell <carol.halliwell@metoffice.gov.uk>
2026-06-29 14:35:49 +0100

    Add functionality to calculate and plot mean and rolling mean of spectra

--- src/CSET/loaders/power_spectrum.py
+++ src/CSET/loaders/power_spectrum.py
@@ -99,3 +99,82 @@ def load(conf: Config):
                 model_ids=[model["id"] for model in models],
                 aggregation=False,
             )
+
+    # Create a list of case aggregation types.
+    AGGREGATION_TYPES = ["all", "rolling"]
+
+    # Surface (2D) fields.
+    for atype, field in itertools.product(AGGREGATION_TYPES, conf.SURFACE_FIELDS):
+        if conf.SPECTRUM_SURFACE_FIELD_AGGREGATION[AGGREGATION_TYPES.index(atype)]:
+            yield RawRecipe(
+                recipe=f"generic_surface_power_spectrum_series_mean_{atype}.yaml",
+                variables={
+                    "VARNAME": field,
+                    "MODEL_NAME": [model["name"] for model in models],
+                    "SUBAREA_TYPE": conf.SUBAREA_TYPE if conf.SELECT_SUBAREA else None,
+                    "SUBAREA_EXTENT": conf.SUBAREA_EXTENT
+                    if conf.SELECT_SUBAREA
+                    else None,
+                    "WINDOW_LEN_SURFACE": conf.WINDOW_LEN_SURFACE
+                    if atype == "rolling"
+                    else None,
+                },
+                model_ids=[model["id"] for model in models],
+                aggregation=True,
+            )
+
+    # Pressure level fields.
+    for atype, field, plevel in itertools.product(
+        AGGREGATION_TYPES, conf.PRESSURE_LEVEL_FIELDS, conf.PRESSURE_LEVELS
+    ):
+        if conf.SPECTRUM_PLEVEL_FIELD_AGGREGATION[AGGREGATION_TYPES.index(atype)]:
+            # Build the variables dict *without* WINDOW_LEN_PLEVEL first
+            variables = {
+                "VARNAME": field,
+                "LEVELTYPE": "pressure",
+                "LEVEL": [plevel],
+                "MODEL_NAME": [model["name"] for model in models],
+                "SUBAREA_TYPE": conf.SUBAREA_TYPE if conf.SELECT_SUBAREA else None,
+                "SUBAREA_EXTENT": conf.SUBAREA_EXTENT if conf.SELECT_SUBAREA else None,
+            }
+
+            # Add WINDOW_LEN_PLEVEL *only if* rolling
+            if atype == "rolling":
+                variables["WINDOW_LEN_PLEVEL"] = conf.WINDOW_LEN_PLEVEL
+            else:
+                variables["WINDOW_LEN_PLEVEL"] = None
+
+            yield RawRecipe(
+                recipe=f"generic_level_power_spectrum_series_plevel_mean_{atype}.yaml",
+                variables=variables,
+                model_ids=[model["id"] for model in models],
+                aggregation=True,
+            )
+
+    # Model level fields.
+    for atype, field, mlevel in itertools.product(
+        AGGREGATION_TYPES, conf.MODEL_LEVEL_FIELDS, conf.MODEL_LEVELS
+    ):
+        if conf.POWER_SPECTRUM_MLEVEL_FIELD_AGGREGATION[AGGREGATION_TYPES.index(atype)]:
+            # Build the variables dict *without* WINDOW_LEN_MLEVEL first
+            variables = {
+                "VARNAME": field,
+                "LEVELTYPE": "model_level_number",
+                "LEVEL": [mlevel],
+                "MODEL_NAME": [model["name"] for model in models],
+                "SUBAREA_TYPE": conf.SUBAREA_TYPE if conf.SELECT_SUBAREA else None,
+                "SUBAREA_EXTENT": conf.SUBAREA_EXTENT if conf.SELECT_SUBAREA else None,
+            }
+
+            # Add WINDOW_LEN_MLEVEL *only if* rolling
+            if atype == "rolling":
+                variables["WINDOW_LEN_MLEVEL"] = conf.WINDOW_LEN_MLEVEL
+            else:
+                variables["WINDOW_LEN_MLEVEL"] = None
+
+            yield RawRecipe(
+                recipe=f"generic_level_power_spectrum_series_mlevel_mean_{atype}.yaml",
+                variables=variables,
+                model_ids=[model["id"] for model in models],
+                aggregation=True,
+            )
