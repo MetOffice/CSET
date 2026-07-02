545cb630c484b2c43790cbec03b6e96205ae08a6
David Flack <77390156+daflack@users.noreply.github.com>
2026-07-02 15:21:56 +0100

    Update docstrings
    Co-authored-by: James Warner <62252918+jwarner8@users.noreply.github.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -741,10 +741,9 @@ def precalc_wind_below_maul_5d(precalc_wind_below_maul_5d_read_only):
     return precalc_wind_below_maul_5d_read_only.copy()
 
 
-# EDIT FROM HERE
 @pytest.fixture()
 def precalc_direction_shear_read_only():
-    """Get precalculated directional shear across maul 3D data. It is NOT safe to modify."""
+    """Get precalculated directional shear across maul for 3D data. It is NOT safe to modify."""
     return read.read_cube("tests/test_data/precipitation/precalc_dir_shear.nc")
 
 
@@ -756,7 +755,7 @@ def precalc_direction_shear(precalc_direction_shear_read_only):
 
 @pytest.fixture()
 def precalc_direction_shear_4d_time_read_only():
-    """Get precalculated directional shear across maul 4D data varying time. It is NOT safe to modify."""
+    """Get precalculated directional shear across maul for 4D data varying time. It is NOT safe to modify."""
     return read.read_cube("tests/test_data/precipitation/precalc_dir_shear_time.nc")
 
 
@@ -768,7 +767,7 @@ def precalc_direction_shear_4d_time(precalc_direction_shear_4d_time_read_only):
 
 @pytest.fixture()
 def precalc_direction_shear_4d_realization_read_only():
-    """Get precalculated directional shear across maul 4D data varying realization. It is NOT safe to modify."""
+    """Get precalculated directional shear across maul for 4D data varying realization. It is NOT safe to modify."""
     return read.read_cube(
         "tests/test_data/precipitation/precalc_dir_shear_realization.nc"
     )
@@ -784,7 +783,7 @@ def precalc_direction_shear_4d_realization(
 
 @pytest.fixture()
 def precalc_direction_shear_5d_read_only():
-    """Get precalculated directional_shear_across maul 5D data. It is NOT safe to modify."""
+    """Get precalculated directional_shear_across maul for 5D data. It is NOT safe to modify."""
     return read.read_cube("tests/test_data/precipitation/precalc_dir_shear_5d.nc")
 
 
