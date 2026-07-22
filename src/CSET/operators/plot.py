d313eb75657b407e5aca7754b23d069277efea06
ukmo-huw-lewis <48992503+ukmo-huw-lewis@users.noreply.github.com>
2026-07-22 21:23:26 +0100

    Add hexbin flag for scatter plot type selection
    Added a hexbin flag to control the type of scatter plot output.

--- src/CSET/operators/plot.py
+++ src/CSET/operators/plot.py
@@ -1719,6 +1719,8 @@ def _plot_and_save_scatter_series(
         minimum for colorbar
     vmax: float
         maximum for colorbar
+    hexbin: bool
+        Flag to set output scatter genereated as a hexbin frequency distribution plot, else scatter of all points.
     """
     fig = plt.figure(figsize=(10, 10), facecolor="w", edgecolor="k")
     ax = plt.gca()
