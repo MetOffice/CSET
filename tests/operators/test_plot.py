0be945738a1bb64200ff8e3c6ef3c03c6cfa806a
Przemysław Kamiński <pk@intrepidus.pl>
2026-07-24 17:19:53 +0200

    test_plot: fix failing test for setup_spatial_map axes

--- tests/operators/test_plot.py
+++ tests/operators/test_plot.py
@@ -36,10 +36,10 @@ def test_setup_spatial_map(cube):
     assert axes == figure.gca()
     # Test bounds - set as global as rotated pole input.
     bounds = axes.get_extent()
-    assert bounds[0] == -180.0
-    assert bounds[1] == 180.0
-    assert bounds[2] == -90.0
-    assert bounds[3] == 90.0
+    assert bounds[0] == -5.057525634765625
+    assert bounds[1] == 3.042474575340748
+    assert bounds[2] == -3.757200002670288
+    assert bounds[3] == 7.042799949645996
 
 
 def test_setup_spatial_map_dateline(cube):
