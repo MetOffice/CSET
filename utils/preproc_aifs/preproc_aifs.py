7d3387ff80a4d45876af4054fd3b51ac5614ccb7
James Warner <james.warner@metoffice.gov.uk>
2026-08-05 07:26:25 +0100

    add dict to support translations

--- utils/preproc_aifs/preproc_aifs.py
+++ utils/preproc_aifs/preproc_aifs.py
@@ -1,23 +1,77 @@
 """do something"""
 
-from collections import defaultdict
+import argparse
+import os
+import subprocess
+from glob import glob
 
 import iris
 import numpy as np
 from iris.coords import DimCoord
 from iris.cube import CubeList
-import os
 
-def combine_ensemble_cubes(cubes):
+
+def fix_name_and_units(cube):
+    """Fix AIFS name and units, where relevant."""
+    aifs_to_lfric_dict = {
+        "2 metre dewpoint temperature": ["dew_point_temperature_at_screen_level", None],
+        "Skin temperature": ["grid_surface_temperature", None],
+        "Snowfall water equivalent": ["grid_surface_snow_amount", None],
+        "Surface long-wave (thermal) radiation downwards": [
+            "surface_downward_clear_longwave_flux_radiative_timestep",
+            None,
+        ],
+        "Total Cloud Cover": ["area_cloud_fraction", None],
+        "Total Precipitation": ["surface_precipitation_amount", None],
+        "Total column water": ["atmosphere_mass_content_of_water_vapor", None],
+        "Mean sea level pressure": ["air_pressure_at_mean_sea_level", None],
+        "2 metre temperature": ["temperature_at_screen_level", None],
+        "Temperature": ["temperature_at_pressure_levels", None],
+        "10 metre U wind component": ["eastward_wind_at_10m", None],
+        "10 metre V wind component": ["northward_wind_at_10m", None],
+        "U component of wind": ["zonal_wind_at_pressure_levels", None],
+        "V component of wind": ["meridional_wind_at_pressure_levels", None],
+        "Geopotential": ["geopotential_height_at_pressure_levels", "m"],
+        "Vertical velocity": ["vertical_wind_at_pressure_levels", None],
+        "Specific humidity": [
+            "vapour_specific_humidity_at_pressure_levels_for_climate_averaging",
+            None,
+        ],
+        "Surface pressure": ["surface_air_pressure", None],
+        "Surface short-wave (solar) radiation downwards": [
+            "surface_direct_shortwave_flux_radiative_timestep",
+            None,
+        ],
+    }
+
+    long_name = cube.long_name
+
+    if long_name in aifs_to_lfric_dict:
+        new_name, new_units = aifs_to_lfric_dict[long_name]
+
+        # Update long_name
+        cube.long_name = new_name
+        cube.rename(new_name)
+
+        # Convert units if required
+        if new_units is not None:
+            if str(cube.units) == "m**2 s**-2" and str(new_units) == "m":
+                cube = cube.copy(data=cube.lazy_data() / 9.80665)
+                cube.units = "m"
+            else:
+                cube.convert_units(new_units)
+
+    return cube
+
+
+def fix_ensemble_cubes(cubes):
     """
     Convert control and perturbed ensemble cubes into a common
     realization dimension and concatenate into a single cube per variable.
     """
-
     processed = CubeList()
 
     for cube in cubes:
-
         cube = cube.copy()
 
         # Remove attributes that prevent concatenation
@@ -27,7 +81,6 @@ def combine_ensemble_cubes(cubes):
             pressure_coord = cube.coord("pressure_level")
 
             if not isinstance(pressure_coord, DimCoord):
-
                 pressure_dim = cube.coord_dims(pressure_coord)[0]
 
                 # Sort pressure values
@@ -52,10 +105,8 @@ def combine_ensemble_cubes(cubes):
                     pressure_dim,
                 )
 
-
         # Perturbed members
         if cube.coords("ensemble_member"):
-
             ensemble_coord = cube.coord("ensemble_member")
 
             cube.remove_coord("ensemble_member")
@@ -71,7 +122,6 @@ def combine_ensemble_cubes(cubes):
 
         # Control member
         else:
-
             # Add a new dimension of length 1
             cube = iris.util.new_axis(cube)
 
@@ -92,60 +142,177 @@ def combine_ensemble_cubes(cubes):
         processed.append(cube)
 
     processed = processed.concatenate()
-    print(processed[0])
-    quit()
 
-    # Concatenate works, i.e. below. Now need to fix metadata (names, ensure time descriptive with forecast_ref, forecast_period etc.)
-
-# air_pressure_at_mean_sea_level / (Pa) (time: 2; realization: 51; latitude: 721; longitude: 1440)
-#     Dimension coordinates:
-#         time                               x               -             -               -
-#         realization                        -               x             -               -
-#         latitude                           -               -             x               -
-#         longitude                          -               -             -               x
-#     Attributes:
-#         Conventions                   'CF-1.6'
-
-# 0: 2 metre dewpoint temperature / (K)  (time: 1; realization: 51; latitude: 721; longitude: 1440)
-# 1: Runoff water equivalent (surface plus subsurface) / (kg m**-2) (time: 1; realization: 51; latitude: 721; longitude: 1440)
-# 2: Skin temperature / (K)              (time: 1; realization: 51; latitude: 721; longitude: 1440)
-# 3: Snowfall water equivalent / (kg m**-2) (time: 1; realization: 51; latitude: 721; longitude: 1440)
-# 4: Surface long-wave (thermal) radiation downwards / (J m**-2) (time: 1; realization: 51; latitude: 721; longitude: 1440)
-# 5: Total Cloud Cover / (%)             (time: 1; realization: 51; latitude: 721; longitude: 1440)
-# 6: Total Precipitation / (kg m**-2)    (time: 1; realization: 51; latitude: 721; longitude: 1440)
-# 7: Total column water / (kg m**-2)     (time: 1; realization: 51; latitude: 721; longitude: 1440)
-# 8: air_pressure_at_mean_sea_level / (Pa) (time: 1; realization: 51; latitude: 721; longitude: 1440)
-# 9: air_temperature / (K)               (time: 1; realization: 51; latitude: 721; longitude: 1440)
-# 10: air_temperature / (K)               (time: 1; realization: 51; pressure_level: 13; latitude: 721; longitude: 1440)
-# 11: eastward_wind / (m s**-1)           (time: 1; realization: 51; latitude: 721; longitude: 1440)
-# 12: eastward_wind / (m s**-1)           (time: 1; realization: 51; latitude: 721; longitude: 1440)
-# 13: eastward_wind / (m s**-1)           (time: 1; realization: 51; pressure_level: 13; latitude: 721; longitude: 1440)
-# 14: geopotential / (m**2 s**-2)         (time: 1; realization: 51; pressure_level: 13; latitude: 721; longitude: 1440)
-# 15: lagrangian_tendency_of_air_pressure / (Pa s**-1) (time: 1; realization: 51; pressure_level: 13; latitude: 721; longitude: 1440)
-# 16: northward_wind / (m s**-1)          (time: 1; realization: 51; latitude: 721; longitude: 1440)
-# 17: northward_wind / (m s**-1)          (time: 1; realization: 51; latitude: 721; longitude: 1440)
-# 18: northward_wind / (m s**-1)          (time: 1; realization: 51; pressure_level: 13; latitude: 721; longitude: 1440)
-# 19: specific_humidity / (kg kg**-1)     (time: 1; realization: 51; pressure_level: 13; latitude: 721; longitude: 1440)
-# 20: surface_air_pressure / (Pa)         (time: 1; realization: 51; latitude: 721; longitude: 1440)
-# 21: surface_downwelling_shortwave_flux_in_air / (J m**-2) (time: 1; realization: 51; latitude: 721; longitude: 1440)
- 
-    # Group by variable name
-    grouped = defaultdict(CubeList)
-
-    for cube in processed:
-        grouped[cube.name()].append(cube)
-
-    # Concatenate control + perturbed members
-    result = CubeList()
-
-    for cubelist in grouped.values():
-        result.append(cubelist.concatenate_cube())
-
-    return result
+    return processed
 
 
-orig = iris.load(os.environ['DATADIR']+'/CSET_testdata/aifs/test/*.nc','air_pressure_at_mean_sea_level')
-#print(orig)
-cubes = combine_ensemble_cubes(orig)
+def fix_time_and_meta(cubes):
 
-print(cubes)
+    done_cubes = iris.cube.CubeList()
+
+    for cube in cubes:
+        # Get a copy of time coord, and dimension this corresponds to.
+        time_coord = cube.coord("time")
+
+        # Work out units of forecast_period and adjust if necessary.
+        units_str = str(time_coord.units)
+
+        # Forecast periods relative to initialisation.
+        fp_points = time_coord.points - time_coord.points[0]
+
+        if units_str.startswith("seconds since"):
+            fp_points = fp_points / 3600.0
+            fp_units = "hours"
+        elif units_str.startswith("minutes since"):
+            fp_points = fp_points / 60.0
+            fp_units = "hours"
+        elif units_str.startswith("hours since"):
+            fp_units = "hours"
+        else:
+            raise ValueError(f"Unhandled time units: {time_coord.units}")
+
+        # Get copy of time coordinate
+        time_coord_points = time_coord.points.copy()
+        time_dim = cube.coord_dims("time")[0]
+
+        # Create forecast period dimension
+        fp_coord = iris.coords.DimCoord(
+            fp_points,
+            standard_name="forecast_period",
+            units=fp_units,
+        )
+
+        # Remove time dimension temporarily, as forecast_period will be lead dimension
+        cube.remove_coord("time")
+        cube.add_dim_coord(fp_coord, time_dim)
+
+        # Add auxiliary forecast initialisation dimension
+        cube.add_aux_coord(
+            iris.coords.AuxCoord(
+                time_coord_points[0],
+                standard_name="forecast_reference_time",
+                units=time_coord.units,
+            )
+        )
+
+        # Create auxiliary time dimension of valid time.
+        new_time_coord = iris.coords.AuxCoord(
+            time_coord_points,
+            standard_name="time",
+            units=time_coord.units,
+        )
+
+        # Add this dimsnion to the cube, tied to the forecast_period dimension.
+        cube.add_aux_coord(
+            new_time_coord,
+            data_dims=(cube.coord_dims("forecast_period")[0],),
+        )
+
+        cube = fix_name_and_units(cube)
+
+        # Append slice to cutout list, ready for saving.
+        done_cubes.append(cube)
+        print(f"{cube.name}...done.")
+
+    return done_cubes
+
+
+# orig = iris.load(os.environ['DATADIR']+'/CSET_testdata/aifs/test/*.nc')
+# #print(orig)
+# cubes = combine_ensemble_cubes(orig)
+
+# print(cubes)
+
+
+def run_in_shell_grib_tools(inputpath, outpath):
+    """
+    Calls to shell grib tools
+
+    need to split out levels first, as
+
+    ECCODES ERROR   :  Cannot handle fields for different levtypes.
+
+    ECCODES ERROR   :  Please split input data into different files. Exiting!
+    """
+    for file in glob(inputpath):
+        for typeOfLevel in [
+            "isobaricInhPa",
+            "heightAboveGround",
+            "surface",
+            "meanSea",
+            "entireAtmosphere",
+        ]:
+            print(f"proc file {file} for typeOfLevel {typeOfLevel}")
+
+            name = os.path.splitext(os.path.basename(file))[0]
+
+            subprocess.check_output(
+                [
+                    "grib_copy",
+                    "-w",
+                    "typeOfLevel=" + typeOfLevel,
+                    file,
+                    outpath + "." + name + "_extract_" + typeOfLevel + ".grib2",
+                ]
+            )
+            subprocess.check_output(
+                [
+                    "grib_to_netcdf",
+                    outpath + "." + name + "_extract_" + typeOfLevel + ".grib2",
+                    "-o",
+                    outpath + "." + name + "_extract_" + typeOfLevel + ".nc",
+                ]
+            )
+            subprocess.check_output(
+                ["rm", outpath + "." + name + "_extract_" + typeOfLevel + ".grib2"]
+            )
+
+
+def main():
+    """TODO"""
+    # Args
+    # Path to data, as a glob.
+    # Date of forecast, for use in filename.
+    # Path to output data.
+
+    """TODO
+    """
+    parser = argparse.ArgumentParser(description="Process arguments.")
+
+    parser.add_argument("--inputpath", required=True, help="Path to file(s) to load.")
+    parser.add_argument(
+        "--forecastinit",
+        type=str,
+        required=True,
+        help="Forecast initialisation to datestamp output.",
+    )
+    parser.add_argument(
+        "--outpath",
+        type=str,
+        required=True,
+        help="Path to save intermediate/final output data.",
+    )
+
+    args = parser.parse_args()
+
+    # Populate required variables
+    inputpath = args.inputpath
+    forecastinit = args.forecastinit
+    outpath = args.outpath + "/"
+
+    print()
+    print("Starting preproc_aifs.py...")
+    print()
+    print("Running preprocess with grib_copy and grib_to_netcdf")
+    run_in_shell_grib_tools(inputpath, outpath)
+
+    cubes = fix_ensemble_cubes(iris.load(outpath + "/.*.nc"))
+    cubes = fix_time_and_meta(cubes)
+
+    iris.save(cubes, outpath + "/AIFS_" + forecastinit + ".nc")
+    for f in glob(outpath + "/.*.nc"):
+        os.remove(f)
+
+
+if __name__ == "__main__":
+    main()
