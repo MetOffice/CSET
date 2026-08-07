57cf8cee4adc6ac0f28ccc8728f60ab097ea43d1
James Frost <james.frost@metoffice.gov.uk>
2026-08-07 12:45:31 +0100

    Remove reference to environment variables and tidy CLI example

--- docs/source/reference/gallery/examples/spatial/plot_surface_spatial.py
+++ docs/source/reference/gallery/examples/spatial/plot_surface_spatial.py
@@ -17,15 +17,16 @@ Using *cset bake* on the command line
 -------------------------------------
 
 * Access recipe file using ``cset cookbook``.
-* Set required recipe inputs on command line (or as environment variables for greater flexibility).
-* Example to generate full-domain spatial maps of ``VARNAME`` for all output times::
+* Set required recipe inputs on command line.
+
+Example to generate full-domain spatial maps of ``temperature_at_screen_level`` for all output times::
 
     cset cookbook generic_surface_spatial_plot_sequence.yaml
     cset -v bake -i "/path/to/input/data" -o "./output_path" \\
                  -r generic_surface_spatial_plot_sequence.yaml \\
                  --VARNAME="temperature_at_screen_level" \\
                  --MODEL_NAME="my_model_label" \\
-                 --METHOD="" \\
+                 --METHOD="SEQ" \\
                  --SUBAREA_TYPE='None' --SUBAREA_EXTENT='None' --SUBAREA_NAME='None'
 
 Configuring the *cset_workflow*
@@ -35,7 +36,7 @@ Configuring the *cset_workflow*
 * Complete ``General setup options`` and ``Cycling and Model options`` details - see :doc:`/usage/workflow-configure`.
 * Set required configuration options on ``Diagnostics / Surface (2D) fields`` panel::
 
-    SURFACE_FIELDS = ['temperature_at_screen_level', ...]
+    SURFACE_FIELDS = ["temperature_at_screen_level", ...]
     SPATIAL_SURFACE_FIELD = True
 
 Example python code
