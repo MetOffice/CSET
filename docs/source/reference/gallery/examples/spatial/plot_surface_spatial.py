ca9485c2560b1b6a8866e35781c1307981eba0b2
James Frost <james.frost@metoffice.gov.uk>
2026-08-07 12:45:24 +0100

    Put literal block markers on end of previous paragraphs
    These get rendered as a single colon and make the raw rst syntax nicer
    to read.
    
    See https://www.sphinx-doc.org/en/master/usage/restructuredtext/basics.html#literal-blocks

--- docs/source/reference/gallery/examples/spatial/plot_surface_spatial.py
+++ docs/source/reference/gallery/examples/spatial/plot_surface_spatial.py
@@ -13,15 +13,12 @@ Generate spatial map of a 2D field (regional data example).
    * :py:mod:`CSET.operators.read.read_cubes`
    * :py:mod:`CSET.operators.plot.spatial_pcolormesh_plot` or :py:mod:`CSET.operators.plot.spatial_contour_plot`.
 
-
-
 Using *cset bake* on the command line
 -------------------------------------
 
 * Access recipe file using ``cset cookbook``.
 * Set required recipe inputs on command line (or as environment variables for greater flexibility).
-* Example to generate full-domain spatial maps of ``VARNAME`` for all output times:
-::
+* Example to generate full-domain spatial maps of ``VARNAME`` for all output times::
 
     cset cookbook generic_surface_spatial_plot_sequence.yaml
     cset -v bake -i "input_data_path" -o "my_output_path" \\
@@ -31,18 +28,15 @@ Using *cset bake* on the command line
                  --METHOD="" \\
                  --SUBAREA_TYPE='None' --SUBAREA_EXTENT='None' --SUBAREA_NAME='None'
 
-
 Configuring the *cset_workflow*
 -------------------------------
 
 * Update workflow configuration settings via ``rose edit`` GUI or in ``rose-suite.conf`` file.
 * Complete ``General setup options`` and ``Cycling and Model options`` details - see :doc:`/usage/workflow-configure`.
-* Set required configuration options on ``Diagnostics / Surface (2D) fields`` panel:
-::
+* Set required configuration options on ``Diagnostics / Surface (2D) fields`` panel::
 
     SURFACE_FIELDS = ['temperature_at_screen_level', ...]
     SPATIAL_SURFACE_FIELD = True
-
 
 Example python code
 -------------------
