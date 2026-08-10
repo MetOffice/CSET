e9617d9c6815d52dc87d726f4d3241e2d4fe9b25
James Ross <james.ross@metoffice.gov.uk>
2026-08-10 09:08:39 +0100

    small change to recipes

--- src/CSET/loaders/verification.py
+++ src/CSET/loaders/verification.py
@@ -170,6 +170,7 @@ def load(conf: Config):
     scores_timeseries_methods_model_vs_obs = (
         _get_scores_timeseries_methods_model_vs_obs(conf)
     )
+
     if scores_timeseries_methods_model_vs_obs:
         # Produce model vs observation timeseries plots of scores metrics averaged over the domain for each case study.
         for model, field, scores_method in itertools.product(
@@ -192,22 +193,48 @@ def load(conf: Config):
 
     scores_spatial_methods_model_vs_obs = _get_scores_spatial_methods_model_vs_obs(conf)
     if scores_spatial_methods_model_vs_obs:
-        # Produce model vs observation timeseries plots of scores metrics averaged over the domain for each case study.
-        for model, field, scores_method in itertools.product(
-            models, conf.SURFACE_FIELDS, scores_spatial_methods_model_vs_obs
+        # Produce 2D spatial plots of scores metrics.
+
+        for model, field, method, scores_method in itertools.product(
+            models,
+            conf.SURFACE_FIELDS_MODEL,
+            conf.SPATIAL_SCORES_FIELD_METHOD_MODEL_VS_OBS,
+            scores_spatial_methods_model_vs_obs,
         ):
+            preserved_coords = ["time", "grid_latitude", "grid_longitude"]
+            method_null = ""
+            scores_method_case = "CASE"
+            scores_coords_case = ["grid_latitude", "grid_longitude"]
+            if scores_method == "RMSE" and method == scores_method_case:
+                # Set the preserved coords and collapse method required
+                # to produce RMSE spatial plot over an entire case study.
+                preserved_coords = scores_coords_case
+                method = method_null
+            if scores_method == "MAE" and method == scores_method_case:
+                # Set the preserved coords and collapse method required
+                # to produce MAE spatial plot over an entire case study.
+                preserved_coords = scores_coords_case
+                method = method_null
+            if scores_method == "additive_bias" and method == scores_method_case:
+                # Set the preserved coords and collapse method required
+                # to produce ME additive bias spatial plot over an entire case study.
+                preserved_coords = scores_coords_case
+                method = method_null
             yield RawRecipe(
-                recipe=f"spatial_surface_difference_scores_model_vs_obs_{scores_method}.yaml",
+                recipe=f"surface_difference_scores_{scores_method}.yaml",
                 variables={
                     "VARNAME": field,
-                    "MODEL_NAMES": models,
+                    "BASE_MODEL": base_model["name"],
+                    "OTHER_MODEL": model["name"],
+                    "METHOD": method,
+                    "PRESERVED_COORDS": preserved_coords,
                     "SUBAREA_NAME": conf.SUBAREA_NAME if conf.SELECT_SUBAREA else "",
                     "SUBAREA_TYPE": conf.SUBAREA_TYPE if conf.SELECT_SUBAREA else None,
                     "SUBAREA_EXTENT": conf.SUBAREA_EXTENT
                     if conf.SELECT_SUBAREA
                     else None,
                 },
-                model_ids=[model["id"]],
+                model_ids=[base_model["id"], model["id"]],
                 aggregation=False,
             )
 
