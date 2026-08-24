17fe7478d8b68e6a6179dbd700d8e858bf27f91f
James Ross <james.ross@metoffice.gov.uk>
2026-08-24 12:09:34 +0100

    adding new scores to gui

--- src/CSET/loaders/verification.py
+++ src/CSET/loaders/verification.py
@@ -54,13 +54,12 @@ def _get_scores_timeseries_methods_model_vs_obs(conf):
 
     if conf.SCORES_TIMESERIES_RMSE_MODEL_VS_OBS or conf.SCORES_ALL_MODEL_VS_OBS:
         scores_timeseries_methods_model_vs_obs.append("RMSE")
-    # TODO: uncomment remainder when backend code has been written
-    # if conf.SCORES_TIMESERIES_AB_MODEL_VS_OBS or conf.SCORES_ALL_MODEL_VS_OBS:
-    #    scores_timeseries_methods_model_vs_obs.append("additive_bias")
-    # if conf.SCORES_TIMESERIES_MAE_MODEL_VS_OBS or conf.SCORES_ALL_MODEL_VS_OBS:
-    #    scores_timeseries_methods_model_vs_obs.append("MAE")
-    # if conf.SCORES_TIMESERIES_PC_MODEL_VS_OBS or conf.SCORES_ALL_MODEL_VS_OBS:
-    #    scores_timeseries_methods_model_vs_obs.append("correlation_pearsonr")
+    if conf.SCORES_TIMESERIES_AB_MODEL_VS_OBS or conf.SCORES_ALL_MODEL_VS_OBS:
+        scores_timeseries_methods_model_vs_obs.append("additive_bias")
+    if conf.SCORES_TIMESERIES_MAE_MODEL_VS_OBS or conf.SCORES_ALL_MODEL_VS_OBS:
+        scores_timeseries_methods_model_vs_obs.append("MAE")
+    if conf.SCORES_TIMESERIES_PC_MODEL_VS_OBS or conf.SCORES_ALL_MODEL_VS_OBS:
+        scores_timeseries_methods_model_vs_obs.append("correlation_pearsonr")
     return scores_timeseries_methods_model_vs_obs
 
 
@@ -83,11 +82,10 @@ def _get_scores_spatial_methods_model_vs_obs(conf):
     scores_spatial_methods_model_vs_obs = []
     if conf.SCORES_SPATIAL_RMSE_MODEL_VS_OBS or conf.SCORES_ALL_MODEL_VS_OBS:
         scores_spatial_methods_model_vs_obs.append("RMSE")
-    # TODO: uncomment remainder when backend code has been written
-    # if conf.SCORES_SPATIAL_AB_MODEL_VS_OBS or conf.SCORES_ALL_MODEL_VS_OBS:
-    # scores_spatial_methods_model_vs_obs.append("additive_bias")
-    # if conf.SCORES_SPATIAL_MAE_MODEL_VS_OBS or conf.SCORES_ALL_MODEL_VS_OBS:
-    # scores_spatial_methods_model_vs_obs.append("MAE")
+    if conf.SCORES_SPATIAL_AB_MODEL_VS_OBS or conf.SCORES_ALL_MODEL_VS_OBS:
+        scores_spatial_methods_model_vs_obs.append("additive_bias")
+    if conf.SCORES_SPATIAL_MAE_MODEL_VS_OBS or conf.SCORES_ALL_MODEL_VS_OBS:
+        scores_spatial_methods_model_vs_obs.append("MAE")
     return scores_spatial_methods_model_vs_obs
 
 
@@ -211,7 +209,10 @@ def load(conf: Config):
     scores_spatial_methods_model_vs_obs = _get_scores_spatial_methods_model_vs_obs(conf)
     if scores_spatial_methods_model_vs_obs:
         # Produce 2D spatial plots of scores metrics.
-
+        preserved_coords = ["time", "grid_latitude", "grid_longitude"]
+        method_null = ""
+        scores_method_case = "CASE"
+        scores_coords_case = ["grid_latitude", "grid_longitude"]
         for field, model, method, scores_method in itertools.product(
             conf.POINT_OBS_FIELDS,
             models,
@@ -223,17 +224,16 @@ def load(conf: Config):
             if scores_method == "RMSE" and method == "CASE":
                 preserved_coords = ["latitude", "longitude"]
                 recipe_method = ""
-            # TODO include these when backend code is added
-            # if scores_method == "MAE" and method == scores_method_case:
-            # Set the preserved coords and collapse method required
-            # to produce MAE spatial plot over an entire case study.
-            #   preserved_coords = scores_coords_case
-            #  method = method_null
-            # if scores_method == "additive_bias" and method == scores_method_case:
-            # Set the preserved coords and collapse method required
-            # to produce ME additive bias spatial plot over an entire case study.
-            #   preserved_coords = scores_coords_case
-            #   method = method_null
+            if scores_method == "MAE" and method == scores_method_case:
+                # Set the preserved coords and collapse method required
+                # to produce MAE spatial plot over an entire case study.
+                preserved_coords = scores_coords_case
+                method = method_null
+            if scores_method == "additive_bias" and method == scores_method_case:
+                # Set the preserved coords and collapse method required
+                # to produce ME additive bias spatial plot over an entire case study.
+                preserved_coords = scores_coords_case
+                method = method_null
 
             yield RawRecipe(
                 recipe=f"surface_scores_model_vs_obs_{scores_method}.yaml",
