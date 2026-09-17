6d06605c1b6acab4b6b4e4cfafba789a5b910106
Bernard Claxton <bernie.claxton@metoffice.gov.uk>
2026-09-17 16:20:47 +0100

    Radar loader for model common domain loops over models #2113

--- src/CSET/loaders/radar.py
+++ src/CSET/loaders/radar.py
@@ -122,7 +122,7 @@ def load(conf: Config):
     # The different sources of Nimrod rainfall accumulation have
     # different spatial grids. So each source requires its own
     # recipe to prevent incompatible cubes being created.
-    if conf.NIMROD_RADAR_OBS and conf.PROCESS_RADAR_2D and (len(accum_radars) > 0):
+    if conf.NIMROD_RADAR_OBS and conf.PROCESS_RADAR_2D and (len(radar_sources) > 0):
         for radar in radar_sources:
             yield RawRecipe(
                 recipe="generic_surface_spatial_plot_sequence_radar_rainfall.yaml",
@@ -161,7 +161,8 @@ def load(conf: Config):
                 "SUBAREA_EXTENT": conf.SUBAREA_EXTENT if conf.SELECT_SUBAREA else None,
                 "SUBAREA_NAME": conf.SUBAREA_NAME if conf.SELECT_SUBAREA else "",
             },
-            model_ids=[model_ids_list[0], "Nimrod2km", "Nimrod2km_weights"],
+            # model_ids=[model_ids_list[0], "Nimrod2km", "Nimrod2km_weights"],
+            model_ids=[model_ids_list[0], radar_obs_ids[0], radar_wts_ids[0]],
             aggregation=False,
         )
 
@@ -172,23 +173,28 @@ def load(conf: Config):
         radar_wts_ids = [radar_source + "_weights"]
         model_names_list = [model["name"] for model in models]
         model_ids_list = [model["id"] for model in models]
-        yield RawRecipe(
-            recipe="model_radar_common_domain_2d_model.yaml",
-            variables={
-                "MODEL_VARNAME": "surface_microphysical_rainfall_rate",
-                "RADAR_VARNAME": "Hourly rain accumulation",
-                "MODEL_LABEL": model_names_list[0],
-                "RADAR_LABEL": radar_obs_ids[0],
-                "MASK_LABEL": radar_wts_ids[0],
-                "METHOD": "SEQ",
-                "BOUNDARY_MARGIN": boundary_margin,
-                "SUBAREA_TYPE": conf.SUBAREA_TYPE if conf.SELECT_SUBAREA else None,
-                "SUBAREA_EXTENT": conf.SUBAREA_EXTENT if conf.SELECT_SUBAREA else None,
-                "SUBAREA_NAME": conf.SUBAREA_NAME if conf.SELECT_SUBAREA else "",
-            },
-            model_ids=[model_ids_list[0], "Nimrod2km", "Nimrod2km_weights"],
-            aggregation=False,
-        )
+        # Loop over the model names.
+        for idx, model_use in enumerate(model_names_list):
+            yield RawRecipe(
+                recipe="model_radar_common_domain_2d_model.yaml",
+                variables={
+                    "MODEL_VARNAME": "surface_microphysical_rainfall_rate",
+                    "RADAR_VARNAME": "Hourly rain accumulation",
+                    "MODEL_LABEL": model_use,
+                    "RADAR_LABEL": radar_obs_ids[0],
+                    "MASK_LABEL": radar_wts_ids[0],
+                    "METHOD": "SEQ",
+                    "BOUNDARY_MARGIN": boundary_margin,
+                    "SUBAREA_TYPE": conf.SUBAREA_TYPE if conf.SELECT_SUBAREA else None,
+                    "SUBAREA_EXTENT": conf.SUBAREA_EXTENT
+                    if conf.SELECT_SUBAREA
+                    else None,
+                    "SUBAREA_NAME": conf.SUBAREA_NAME if conf.SELECT_SUBAREA else "",
+                },
+                # model_ids=[model_ids_list[0], "Nimrod2km", "Nimrod2km_weights"],
+                model_ids=[model_ids_list[idx], radar_obs_ids[0], radar_wts_ids[0]],
+                aggregation=False,
+            )
 
     # Histogram sequence rainfall using common domain between
     # model and Nimrod radar observations.
