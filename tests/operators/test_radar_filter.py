4875f8e2a794b138dc4576f2d6a5caae2c4cb9af
Bernard Claxton <bernie.claxton@metoffice.gov.uk>
2026-09-21 15:41:18 +0100

    Added test for radar_filter.mask_by_weights  #2113

--- tests/operators/test_radar_filter.py
+++ tests/operators/test_radar_filter.py
@@ -14,6 +14,10 @@
 
 """Test filter operators."""
 
+import iris
+import numpy as np
+from iris.cube import Cube
+
 from CSET.operators import constraints, radar_filter
 
 constraint_single = constraints.combine_constraints(
@@ -27,3 +31,34 @@ def test_mask_list():
     list_weights = radar_filter.mask_list(["UM_model", "Nimrod1km", "Nimrod2km"])
     expected_list = ["Nimrod2km_weights", "Nimrod1km_weights", "Nimrod2km_weights"]
     assert list_weights == expected_list
+
+
+def test_mask_by_weights():
+    """Test the mask_by_weights function."""
+    obs = Cube([[1.0, 2.0], [3.0, 4.0]])
+    wts = Cube([[13, 0], [10, 11]])
+    out = Cube([[1.0, np.nan], [np.nan, 4.0]])
+    print("----> 1 obs ", obs)
+
+    var_name_obs = "hourly_rain_accumulation"
+    var_name_wts = "hourly_wts_accumulation"
+
+    obs_name = "Radar"
+    wts_name = "Radar_wts"
+    obs.attributes["model_name"] = obs_name
+    wts.attributes["model_name"] = wts_name
+
+    obs.rename(var_name_obs)
+    obs.long_name = var_name_obs
+    obs.var_name = var_name_obs
+
+    wts.rename(var_name_wts)
+    wts.long_name = var_name_wts
+    wts.var_name = var_name_wts
+
+    cube_list = iris.cube.CubeList([])
+    cube_list.append(obs)
+    cube_list.append(wts)
+    masked = radar_filter.mask_by_weights(cube_list, [obs_name], [wts_name])
+
+    assert np.array_equal(masked.data, out.data, equal_nan=True)
