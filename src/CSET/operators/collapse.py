ac935a3b53d0bdc5efd0e6f0fa06e49cf1912b71
Carol Halliwell <150140850+cehalliwell@users.noreply.github.com>
2026-09-21 14:46:22 +0100

    Update src/CSET/operators/collapse.py
    Co-authored-by: James Ross <james.ross@metoffice.gov.uk>

--- src/CSET/operators/collapse.py
+++ src/CSET/operators/collapse.py
@@ -84,23 +84,18 @@ def collapse(
             for coord in ["frequency", "physical_wavenumber", "wavelength"]
         )
         if is_power_spectrum:
-            # Power spectrum cube
-            for cube in cubes:
-                cube.coord("time").bounds = None
-            cubes = cubes.extract_overlapping(["time"])
+            coord_names = ["time"]           
+        else:
+            coord_names = ["forecast_reference_time", "forecast_period"]
+        
+         for coord_name in coord_names:
+             cube.coord(coord_name).bounds = None
+          cubes = cubes.extract_overlapping(coord_names)
 
-            for cube in cubes:
+         if is_power_spectrum:
+             for cube in cubes:
                 t = cube.coord("time")
                 t.points = t.points.astype(np.float64)
-
-        else:
-            # Not power spectrum
-            for cube in cubes:
-                cube.coord("forecast_reference_time").bounds = None
-                cube.coord("forecast_period").bounds = None
-            cubes = cubes.extract_overlapping(
-                ["forecast_reference_time", "forecast_period"]
-            )
 
         if len(cubes) == 0:
             raise ValueError("No overlapping times detected in input cubes.")
