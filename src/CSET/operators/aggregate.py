0b489f6d74289f5650739eb65da5ec5f7cb47503
James Ross <james.ross@metoffice.gov.uk>
2026-09-22 15:40:09 +0100

    Update src/CSET/operators/aggregate.py
    Co-authored-by: James Warner <62252918+jwarner8@users.noreply.github.com>

--- src/CSET/operators/aggregate.py
+++ src/CSET/operators/aggregate.py
@@ -327,7 +327,7 @@ def _aggregate_in_time_multiple_frt(
 
 
 def _aggregate_in_time_single_frt(cube: iris.cube.Cube, method: str, interval: int):
-    # Aggregate a cube with one forecast reference time.
+    """Aggregate a cube with one forecast reference time."""
 
     iris.coord_categorisation.add_categorised_coord(
         cube,
